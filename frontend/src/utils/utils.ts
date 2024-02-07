3a69927fb2d6156af9b214194106ed39c3048fb4
Michael Hunger <github@jexp.de>
2024-02-07 15:54:56 +0000

    Fix Backend URL handling - env variable

--- frontend/src/utils/utils.ts
+++ frontend/src/utils/utils.ts
@@ -1,6 +1,6 @@
 export const url = () => {
   var url = window.location.href.replace("5173","8000");
-  if (process.env.BACKEND_API_URL !== undefined) {
+  if (process.env.BACKEND_API_URL) {
     url = process.env.BACKEND_API_URL;
   }
   return !url || !url.match("/$") ? url : url.substring(0, url.length-1);
