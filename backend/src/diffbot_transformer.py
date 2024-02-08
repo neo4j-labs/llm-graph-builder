841e025419c390a94ae2ac27109743cc628d41d2
aashipandya <156318202+aashipandya@users.noreply.github.com>
2024-02-08 10:32:31 +0000

    diffbot to process one chunk at a time and model value in final json response

--- backend/src/diffbot_transformer.py
+++ backend/src/diffbot_transformer.py
@@ -7,12 +7,24 @@ import os
 
 def extract_graph_from_diffbot(graph: Neo4jGraph, 
                                chunks: List[Document]):
+    """ Create knowledge graph using diffbot transformer
+
+    Args:
+        graph (Neo4jGraph): Neo4jGraph connection object
+        chunks (List[Document]): list of chunk documents created from input file
+
+    Returns: langchain GraphDocument - used to generate graph
+        
+    """    
     diffbot_api_key = os.environ.get('DIFFBOT_API_KEY')
     diffbot_nlp = DiffbotGraphTransformer(diffbot_api_key=diffbot_api_key)
-    graph_documents = diffbot_nlp.convert_to_graph_documents(chunks)
-    graph.add_graph_documents(graph_documents)
-
+    graph_document_list = []
+    
+    for chunk in chunks:
+        graph_document = diffbot_nlp.convert_to_graph_documents([chunk])
+        graph.add_graph_documents(graph_document)
+        graph_document_list.append(graph_document[0]) 
+           
     graph.refresh_schema()
-
-    return graph_documents
+    return graph_document_list
     
