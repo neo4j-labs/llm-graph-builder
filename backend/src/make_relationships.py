8a6f2a6d6cded59030f62c5602a7b1be45be1364
Pravesh Kumar <pravesh_kumar@persistent.com>
2024-02-12 13:31:46 +0530

    Print nodes count on chunk level and accept isEmbedding param added in extract API

--- backend/src/make_relationships.py
+++ backend/src/make_relationships.py
@@ -11,7 +11,7 @@ def create_source_chunk_entity_relationship(source_file_name :str,graph: Neo4jGr
 
     """
     source_node = "fileName: '{}'"
-    print(f'Graph Document print{graph_document}')
+    # print(f'Graph Document print{graph_document}')
     chunk_node_id = "uuid:'{}'"
     update_chunk_node_prop = "SET c.text = '{}'"
     result = graph.query('CREATE(c:Chunk {uuid:randomUUID()}) '+update_chunk_node_prop.format(chunk.page_content)+' RETURN c.uuid AS UUID')
@@ -19,8 +19,17 @@ def create_source_chunk_entity_relationship(source_file_name :str,graph: Neo4jGr
     chunk_uuid = result[0]['UUID']
     #make relationship between chunk node and source node.
     graph.query('MATCH(s:Source {'+source_node.format(source_file_name)+'}) ,(c:Chunk {'+chunk_node_id.format(chunk_uuid)+'}) CREATE (s)-[:HAS_CHILD]->(c)')
-
+    dict = {}
+    nodes_list = []
     for node in graph_document[0].nodes:
         node_id = node.id
         result = graph.query("MATCH(c:Chunk {"+chunk_node_id.format(chunk_uuid)+"}), (n:"+ node.type +"{ id: '"+node_id+"'}) CREATE (c)-[:HAS_ENTITY]->(n)")
-        
+    #     json_obj = {'node_id': node_id, 'node_type' : node.type, 'uuid' : chunk_uuid}
+    #     nodes_list.append(json_obj)
+
+    # dict['chunk_doc'] = chunk.page_content
+    # dict['rel_chunk_entity_node'] = nodes_list
+    # dict['nodes_created_in_chunk'] = len(graph_document[0].nodes)
+    # dict['relationships_created_in_chunk'] = len(graph_document[0].relationships)
+
+    # print(f'dictionary object include nodes and content {dict}')
