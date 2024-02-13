67d0376831b5e0c8f6d831efa4bee9846dc8c372
kartikpersistent <101251502+kartikpersistent@users.noreply.github.com>
2024-02-13 19:33:07 +0000

    fixed extract url

--- frontend/src/services/Extract.ts
+++ frontend/src/services/Extract.ts
@@ -10,7 +10,7 @@ const extractAPI = async (file: any, model: string, userCredentials?: any,) => {
     formData.append('uri', userCredentials?.uri);
     formData.append('userName', userCredentials?.userName);
     formData.append('password', userCredentials?.password);
-    const response = await axios.post(`${url()}extract`, formData, {
+    const response = await axios.post(`${url()}/extract`, formData, {
       headers: {
         'Content-Type': 'multipart/form-data',
       },
