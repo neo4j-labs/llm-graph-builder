3d7f1b09804f619a61f70fd6c04134df91cb7a32
rakshita-arora <118245454+rakshita-arora@users.noreply.github.com>
2024-02-14 21:07:10 +0000

    s3 loaded functionality added

--- backend/score.py
+++ backend/score.py
@@ -4,8 +4,7 @@ from fastapi import FastAPI, Depends
 from fastapi_health import health
 from fastapi.middleware.cors import CORSMiddleware
 from src.main import *
-import asyncio
-
+from src.utilities import *
 
 def healthy_condition():
     output = {"healthy": True}
@@ -19,7 +18,6 @@ def healthy():
 def sick():
     return False
 
-
 app = FastAPI()
 
 app.add_middleware(
@@ -32,70 +30,25 @@ app.add_middleware(
 app.add_api_route("/health", health([healthy_condition, healthy]))
 
 
-@app.post("/sources")
-async def create_source_knowledge_graph(
-    uri=Form(), userName=Form(), password=Form(), file: UploadFile = File(...)
-):
-    """
-    Calls 'create_source_node_graph' function in a new thread to create
-    source node in Neo4jGraph when a new file is uploaded.
+@app.post('/extract')
+async def extract_knowledge_graph_from_file(uri= Form(), userName= Form(), password= Form(), model=Form(),file: UploadFile = File(None),s3_url=Form(None)):
+    if file:
+        return extract_graph_from_file(uri, userName, password, model,file=file,s3_url=None)
+    elif s3_url:
+        return extract_graph_from_file(uri, userName, password, model,s3_url=s3_url)
+    else:
+        return {}
 
-    Args:
-         uri: URI of Graph Service to connect to
-         userName: Username to connect to Graph Service with ( default : None )
-         password: Password to connect to Graph Service with ( default : None )
-         file: File object containing the PDF file
-
-    Returns:
-         'Source' Node creation in Neo4j database
-    """
-    try:
-        result = await asyncio.to_thread(
-            create_source_node_graph, uri, userName, password, file
-        )
-        return result
-    except Exception as e:
-        job_status = "Failure"
-        error_message = str(e)
-        logging.exception(f"Exception Stack trace:{e}")
-        return create_api_response(job_status, error=error_message)
-
-
-@app.post("/extract")
-async def extract_knowledge_graph_from_file(
-    uri=Form(),
-    userName=Form(),
-    password=Form(),
-    file: UploadFile = File(...),
-    model=Form(),
-):
-    """
-    Calls 'extract_graph_from_file' in a new thread to create Neo4jGraph from a
-    PDF file based on the model.
-
-    Args:
-          uri: URI of the graph to extract
-          userName: Username to use for graph creation
-          password: Password to use for graph creation
-          file: File object containing the PDF file
-          model: Type of model to use ('Diffbot'or'OpenAI GPT')
-
-    Returns:
-          Nodes and Relations created in Neo4j databse for the pdf file
-    """
-    try:
-        result = await asyncio.to_thread(
-            extract_graph_from_file, uri, userName, password, file, model
-        )
-        return result
-    except Exception as e:
-        job_status = "Failure"
-        error_message = str(e)
-        logging.exception(f"Exception Stack trace:{e}")
-        return create_api_response(job_status, error=error_message)
-
-
-@app.get("/sources_list")
+# @app.post('/extract')
+# async def extract_knowledge_graph_from_file(uri= Form(), userName= Form(), password= Form(), model=Form(),file: UploadFile = File(None),s3_url=Form(None)):
+#     if file:
+#         return extract_graph_from_file(uri, userName, password, model,file)
+#     elif s3_url:
+#         return extract_graph_from_file(uri, userName, password, model,s3_url=s3_url)
+#     else:
+#         return {}
+    
+@app.get('/sources_list')
 async def get_source_list():
     """
     Calls 'get_source_list_from_graph' which returns list of sources which alreday exist in databse
@@ -108,6 +61,13 @@ async def get_source_list():
         error_message = str(e)
         logging.exception(f"Exception Stack trace:{e}")
         return create_api_response(job_status, error=error_message)
+
+
+@app.post('/bucket/scan')
+async def create_source_knowledge_graph(uri= Form(), userName= Form(), password= Form(),s3_url=Form()):
+    return create_source_node_graph_s3(uri, userName, password, s3_url)
+# async def s3_loader(bucket_url=Form(),model=Form()):
+#     return get_source_list_from_graph()
 
 
 if __name__ == "__main__":
