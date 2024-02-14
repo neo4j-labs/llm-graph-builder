1d68c82c7a2c3064ac0c67759469241d593692db
rakshita-arora <118245454+rakshita-arora@users.noreply.github.com>
2024-02-14 21:07:10 +0000

    s3 loaded functionality added

--- backend/score.py
+++ backend/score.py
@@ -4,6 +4,7 @@ from fastapi import FastAPI, Depends
 from fastapi_health import health
 from fastapi.middleware.cors import CORSMiddleware
 from src.main import *
+from src.utilities import *
 
 def healthy_condition():
     output={"healthy": True}
@@ -15,7 +16,7 @@ def healthy():
 def sick():
     return False
 
-app = FastAPI();
+app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
@@ -31,12 +32,33 @@ async def create_source_knowledge_graph(uri= Form(), userName= Form(), password=
     return create_source_node_graph(uri, userName, password, file)
 
 @app.post('/extract')
-async def extract_knowledge_graph_from_file(uri= Form(), userName= Form(), password= Form(),file: UploadFile = File(...), model=Form()):
-    return extract_graph_from_file(uri, userName, password, file, model)
+async def extract_knowledge_graph_from_file(uri= Form(), userName= Form(), password= Form(), model=Form(),file: UploadFile = File(None),s3_url=Form(None)):
+    if file:
+        return extract_graph_from_file(uri, userName, password, model,file=file,s3_url=None)
+    elif s3_url:
+        return extract_graph_from_file(uri, userName, password, model,s3_url=s3_url)
+    else:
+        return {}
 
+# @app.post('/extract')
+# async def extract_knowledge_graph_from_file(uri= Form(), userName= Form(), password= Form(), model=Form(),file: UploadFile = File(None),s3_url=Form(None)):
+#     if file:
+#         return extract_graph_from_file(uri, userName, password, model,file)
+#     elif s3_url:
+#         return extract_graph_from_file(uri, userName, password, model,s3_url=s3_url)
+#     else:
+#         return {}
+    
 @app.get('/sources_list')
 async def get_source_list():
     return get_source_list_from_graph()
+
+@app.post('/bucket/scan')
+async def create_source_knowledge_graph(uri= Form(), userName= Form(), password= Form(),s3_url=Form()):
+    return create_source_node_graph_s3(uri, userName, password, s3_url)
+# async def s3_loader(bucket_url=Form(),model=Form()):
+#     return get_source_list_from_graph()
+
 
 if __name__ == "__main__":
     uvicorn.run(app)
