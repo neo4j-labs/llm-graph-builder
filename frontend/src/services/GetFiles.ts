e91476735e66ee5645055d3146c8fe329d6fe92c
kartikpersistent <101251502+kartikpersistent@users.noreply.github.com>
2024-03-05 12:29:49 +0000

    fixed encryption bug

--- frontend/src/services/GetFiles.ts
+++ frontend/src/services/GetFiles.ts
@@ -2,13 +2,14 @@ import axios from 'axios';
 import { url } from '../utils/Utils';
 
 export const getSourceNodes = async (userCredentials: any) => {
+  const encodedstr = btoa(userCredentials.password);
   try {
-    const encodedstr = btoa(userCredentials.password);
     const response = await axios.get(
       `${url()}/sources_list?uri=${userCredentials.uri}&userName=${userCredentials.userName}&password=${encodedstr}&selectedProtocol=${userCredentials.selectedProtocol}}`);
     if (response.status != 200) {
       throw new Error('Some error occurred');
     }
+    console.log(response);
     return response;
   } catch (error) {
     return error;
