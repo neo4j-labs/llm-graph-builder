c60bdb7db4bbb3bd90a085d67c2f57624f526927
Prakriti Solankey <156313631+prakriti-solankey@users.noreply.github.com>
2024-03-05 11:06:51 +0000

    password handling

--- frontend/src/services/GetFiles.ts
+++ frontend/src/services/GetFiles.ts
@@ -3,11 +3,9 @@ import { url } from '../utils/Utils';
 
 export const getSourceNodes = async (userCredentials: any) => {
   try {
+    const encodedstr = atob(userCredentials.password);
     const response = await axios.get(
-      `${url()}/sources_list?uri=${userCredentials.uri}&userName=${userCredentials.userName}&password=${atob(
-        userCredentials.password
-      )}`
-    );
+      `${url()}/sources_list?uri=${userCredentials.uri}&userName=${userCredentials.userName}&password=${encodedstr}}`);
     if (response.status != 200) {
       throw new Error('Some error occurred');
     }
