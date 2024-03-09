85dc5ca1711cac2b6a98701980069a0f36700af8
kartikpersistent <101251502+kartikpersistent@users.noreply.github.com>
2024-03-09 04:44:36 +0000

    fixed max limit nan bug

--- frontend/src/utils/FileAPI.ts
+++ frontend/src/utils/FileAPI.ts
@@ -22,14 +22,18 @@ export const extractAPI = async (
   aws_access_key_id?: any,
   aws_secret_access_key?: any,
   max_sources?: number,
-  wiki_query?: string,
+  wiki_query?: string
 ): Promise<any> => {
   const urlExtract = `${url()}/extract`;
   const method: Method = 'post';
   const commonParams: UserCredentials = userCredentials;
   let additionalParams: ExtractParams;
   if (source_url?.length) {
-    additionalParams = { model, source_url, aws_secret_access_key, aws_access_key_id, max_sources, wiki_query };
+    if (max_sources != undefined && !isNaN(max_sources)) {
+      additionalParams = { model, source_url, aws_secret_access_key, aws_access_key_id, max_sources, wiki_query };
+    } else {
+      additionalParams = { model, source_url, aws_secret_access_key, aws_access_key_id, wiki_query };
+    }
   } else {
     additionalParams = { file, model };
   }
