5efacc39a0293f6d91ba223534ee6274b41eda48
kartikpersistent <101251502+kartikpersistent@users.noreply.github.com>
2024-03-13 13:23:31 +0000

    styling fixes for close button

--- frontend/src/components/Chatbot.tsx
+++ frontend/src/components/Chatbot.tsx
@@ -74,7 +74,7 @@ export default function Chatbot(props: ChatbotProps) {
 
     return (
         <div className='n-bg-palette-neutral-bg-weak flex flex-col justify-between min-h-full max-h-full overflow-hidden w-[312px]'>
-            <div className='flex overflow-y-auto pb-12 min-w-full'>
+            <div className='flex overflow-y-auto pb-12 min-w-full' style={{scrollbarWidth:"thin"}}>
                 <Widget className='n-bg-palette-neutral-bg-weak' header='' isElevated={false}>
                     <div className='flex flex-col gap-4 p-3'>
                         {listMessages.map((chat) => (
