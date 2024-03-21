6738b43e26710444e2ae0c496472a3daff9186f2
kartikpersistent <101251502+kartikpersistent@users.noreply.github.com>
2024-03-21 12:53:36 +0000

    added session id

--- frontend/src/services/QnaAPI.ts
+++ frontend/src/services/QnaAPI.ts
@@ -1,15 +1,15 @@
 import axios from 'axios';
 import { url } from '../utils/Utils';
 
-const chatBotAPI = async (userCredentials: any, model: string, question: string) => {
+const chatBotAPI = async (userCredentials: any, question: string, session_id: string) => {
   try {
     const formData = new FormData();
     formData.append('uri', userCredentials?.uri ?? '');
     formData.append('database', userCredentials?.database ?? '');
     formData.append('userName', userCredentials?.userName ?? '');
     formData.append('password', userCredentials?.password ?? '');
-    formData.append('model', model);
     formData.append('question', question);
+    formData.append("session_id", session_id)
     const response: any = await axios.post(`${url()}/chat_bot`, formData, {
       headers: {
         'Content-Type': 'multipart/form-data',
