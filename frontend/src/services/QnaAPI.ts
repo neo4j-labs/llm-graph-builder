ddf09b8e30024ff9437ed665464e0c711966bc2a
kartikpersistent <101251502+kartikpersistent@users.noreply.github.com>
2024-03-21 12:57:40 +0000

    code format

--- frontend/src/services/QnaAPI.ts
+++ frontend/src/services/QnaAPI.ts
@@ -9,7 +9,7 @@ const chatBotAPI = async (userCredentials: any, question: string, session_id: st
     formData.append('userName', userCredentials?.userName ?? '');
     formData.append('password', userCredentials?.password ?? '');
     formData.append('question', question);
-    formData.append("session_id", session_id)
+    formData.append('session_id', session_id);
     const response: any = await axios.post(`${url()}/chat_bot`, formData, {
       headers: {
         'Content-Type': 'multipart/form-data',
