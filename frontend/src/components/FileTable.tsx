88d89754369839b1966ad61e7a9a99dfef24457d
kartikpersistent <101251502+kartikpersistent@users.noreply.github.com>
2024-03-27 12:18:30 +0000

    added bucket name and folder in the filetable

--- frontend/src/components/FileTable.tsx
+++ frontend/src/components/FileTable.tsx
@@ -141,7 +141,7 @@ const FileTable: React.FC<FileTableProps> = ({ isExpanded, connectionStatus, set
           throw new Error('Please check backend connection');
         }
         if (res.data.status !== 'Failed') {
-          const prefiles: any[] = [];
+          const prefiles: CustomFile[] = [];
           if (res.data.data.length) {
             res.data.data.forEach((item: SourceNode) => {
               if (item.fileName != undefined && item.fileName.length) {
@@ -168,6 +168,8 @@ const FileTable: React.FC<FileTableProps> = ({ isExpanded, connectionStatus, set
                   id: uuidv4(),
                   source_url: item.url != 'None' && item?.url != '' ? item.url : '',
                   fileSource: item.fileSource ?? 'None',
+                  gcsBucket:item?.gcsBucket,
+                  gcsBucketFolder:item?.gcsBucketFolder
                 });
               }
             });
