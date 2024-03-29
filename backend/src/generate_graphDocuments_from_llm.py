d728911813749763d2ded4d62bf914e9da22d974
aashipandya <156318202+aashipandya@users.noreply.github.com>
2024-03-29 12:42:31 +0000

    common method to create chunk and gd list

--- backend/src/generate_graphDocuments_from_llm.py
+++ backend/src/generate_graphDocuments_from_llm.py
@@ -9,21 +9,21 @@ import logging
 logging.basicConfig(format="%(asctime)s - %(message)s", level="INFO")
 
 
-def generate_graphDocuments(model: str, graph: Neo4jGraph, lst_chunks: List):
+def generate_graphDocuments(model: str, graph: Neo4jGraph, chunks: List):
     if model == "Diffbot":
-        graph_documents = get_graph_from_diffbot(graph, lst_chunks)
+        graph_documents = get_graph_from_diffbot(graph, chunks)
 
     elif model == "OpenAI GPT 3.5":
         model_version = "gpt-3.5-turbo-16k"
-        graph_documents_chunk_chunk_Id = get_graph_from_OpenAI(model_version, graph, lst_chunks)
+        graph_documents_chunk_chunk_Id = get_graph_from_OpenAI(model_version, graph, chunks)
 
     elif model == "OpenAI GPT 4":
         model_version = "gpt-4-0125-preview"
-        graph_documents_chunk_chunk_Id = get_graph_from_OpenAI(model_version, graph, lst_chunks)
+        graph_documents_chunk_chunk_Id = get_graph_from_OpenAI(model_version, graph, chunks)
     
     elif model == "Gemini Pro" :
         model_version = "gemini-1.0-pro"
-        graph_documents_chunk_chunk_Id = get_graph_from_Gemini(model_version, graph, lst_chunks)
+        graph_documents_chunk_chunk_Id = get_graph_from_Gemini(model_version, graph, chunks)
 
     logging.info(f"graph_documents = {len(graph_documents_chunk_chunk_Id)}")
     return graph_documents_chunk_chunk_Id
