cb992c81c3f1a1dc0c67e491231bfcbf1740e447
kartikpersistent <101251502+kartikpersistent@users.noreply.github.com>
2024-04-01 14:24:51 +0000

    deleted venv folder

--- frontend/src/components/FileTable.tsx
+++ frontend/src/components/FileTable.tsx
@@ -146,7 +146,7 @@ const FileTable: React.FC<FileTableProps> = ({ isExpanded, connectionStatus, set
         if (!res.data) {
           throw new Error('Please check backend connection');
         }
-
+        console.log({res})
         if (res.data.status !== 'Failed') {
           const prefiles: CustomFile[] = [];
           if (res.data.data.length) {
@@ -184,8 +184,7 @@ const FileTable: React.FC<FileTableProps> = ({ isExpanded, connectionStatus, set
           setIsLoading(false);
           setFilesData(prefiles);
           const prefetchedFiles: any[] = [];
-          console.log("res.data.data.data",res.data.data.data)
-          res.data.data.data.forEach((item: any) => {
+          res.data.data.forEach((item: any) => {
             const localFile = getFileFromLocal(`${item.fileName}`);
             if (item.fileName != undefined && item.fileName.length) {
               if (localFile != null) {
