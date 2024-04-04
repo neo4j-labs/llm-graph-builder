f33a62d34486e6e61e4f1b5a59ff773374f3bd49
karanchellani <142801957+karanchellani@users.noreply.github.com>
2024-04-04 10:23:27 +0530

    Update gemini_llm.py

--- backend/src/gemini_llm.py
+++ backend/src/gemini_llm.py
@@ -295,7 +295,8 @@ def get_graph_from_Gemini(model_version,
     graph_document_list = []
     lst_chunk_chunkId_document=[]
     location = "us-central1"
-    credentials, project_id = google.auth.default()
+    project = "llm-experiments-387609"                            
+    #credentials, project_id = google.auth.default()
     vertexai.init(project=project_id, location=location)
     
     # combined_chunk_document_list=[]
