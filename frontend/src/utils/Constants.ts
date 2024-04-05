281f2a0c3e53fb5e02e76971250df652ba992ed1
Prakriti Solankey <156313631+prakriti-solankey@users.noreply.github.com>
2024-04-05 09:45:16 +0000

    LLM Model Changes

--- frontend/src/utils/Constants.ts
+++ frontend/src/utils/Constants.ts
@@ -1,6 +1,6 @@
 export const document = `+ [docs]`;
 
-export const chunks = `+ collect { MATCH p=(c)-[:NEXT_CHUNK]-() RETURN p } // chunk-chain
+export const chunks = `+ [chunks] + collect { MATCH p=(c)-[:NEXT_CHUNK]->() RETURN p } // chunk-chain
 + collect { MATCH p=(c)-[:SIMILAR]-() RETURN p } // similar-chunks`;
 
 export const entities = `+ collect { OPTIONAL MATCH (c:Chunk)-[:HAS_ENTITY]->(e), p=(e)-[*0..1]-(:!Chunk) RETURN p }`;
@@ -8,19 +8,14 @@ export const entities = `+ collect { OPTIONAL MATCH (c:Chunk)-[:HAS_ENTITY]->(e)
 export const docEntities = `+ [docs] 
 + collect { MATCH (c:Chunk)-[:HAS_ENTITY]->(e), p=(e)--(:!Chunk) RETURN p }`;
 
-export const docChunks = `+[chunks]
-+collect {MATCH p=(c)-[:FIRST_CHUNK]-() RETURN p} //first chunk
-+ collect { MATCH p=(c)-[:NEXT_CHUNK]-() RETURN p } // chunk-chain
-+ collect { MATCH p=(c)-[:SIMILAR]-() RETURN p } // similar-chunk`;
-
-export const chunksEntities = `+ collect { MATCH p=(c)-[:NEXT_CHUNK]-() RETURN p } // chunk-chain
+export const docChunks = `+ [chunks] // documents with chunks
++ collect { MATCH p=(c)-[:NEXT_CHUNK]->() RETURN p } // chunk-chain
 + collect { MATCH p=(c)-[:SIMILAR]-() RETURN p } // similar-chunks
-//chunks with entities
-+ collect { OPTIONAL MATCH p=(c:Chunk)-[:HAS_ENTITY]->(e)-[*0..1]-(:!Chunk) RETURN p }`;
++ [docs]`;
 
-export const docChunkEntities = `+[chunks]
-+collect {MATCH p=(c)-[:FIRST_CHUNK]-() RETURN p} //first chunk
-+ collect { MATCH p=(c)-[:NEXT_CHUNK]-() RETURN p } // chunk-chain
+export const chunksEntities = `// if chunks:
++ [chunks] // documents with chunks
++ collect { MATCH p=(c)-[:NEXT_CHUNK]->() RETURN p } // chunk-chain
 + collect { MATCH p=(c)-[:SIMILAR]-() RETURN p } // similar-chunks
 //chunks with entities
 + collect { OPTIONAL MATCH p=(c:Chunk)-[:HAS_ENTITY]->(e)-[*0..1]-(:!Chunk) RETURN p }`;
