f76034c8ae57343ed19900c605ab02ef5ffed469
aashipandya <156318202+aashipandya@users.noreply.github.com>
2024-04-09 07:05:50 +0000

    Gemini security remove and added sleep

--- backend/src/gemini_llm.py
+++ backend/src/gemini_llm.py
@@ -23,6 +23,8 @@ from typing import List, Dict
 from langchain_core.pydantic_v1 import BaseModel, Field
 import google.auth 
 from langchain_community.graphs.graph_document import Node
+import time
+from langchain_google_vertexai import HarmBlockThreshold, HarmCategory
 
 load_dotenv()
 logging.basicConfig(format='%(asctime)s - %(message)s',level='DEBUG')
@@ -310,13 +312,21 @@ def get_graph_from_Gemini(model_version,
         
     llm = ChatVertexAI(model_name=model_version,
                     convert_system_message_to_human=True,
-                    temperature=0
+                    temperature=0,
+                    safety_settings={
+                        HarmCategory.HARM_CATEGORY_UNSPECIFIED: HarmBlockThreshold.BLOCK_NONE, 
+                        HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: HarmBlockThreshold.BLOCK_NONE, 
+                        HarmCategory.HARM_CATEGORY_HATE_SPEECH: HarmBlockThreshold.BLOCK_NONE, 
+                        HarmCategory.HARM_CATEGORY_HARASSMENT: HarmBlockThreshold.BLOCK_NONE, 
+                        HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: HarmBlockThreshold.BLOCK_NONE,
+                    }
                 )
     llm_transformer = GeminiLLMGraphTransformer(llm=llm)
     
     with ThreadPoolExecutor(max_workers=10) as executor:
         for chunk in chunks:
-            futures.append(executor.submit(llm_transformer.convert_to_graph_documents,[chunk]))   
+            chunk_doc = Document(page_content=chunk.page_content.encode("utf-8"), metadata=chunk.metadata)
+            futures.append(executor.submit(llm_transformer.convert_to_graph_documents,[chunk_doc]))   
         
         for i, future in enumerate(concurrent.futures.as_completed(futures)):
             graph_document = future.result()
@@ -333,6 +343,11 @@ def get_graph_from_Gemini(model_version,
                 # replace all non alphanumeric characters and spaces with underscore
                node.type = re.sub(r'[^\w]+', '_', node.type.capitalize())
             graph_document_list.append(graph_document[0])
+            
+            #Sleep for 1 sec after 4 requestes are processed. 
+            # Todo: Remove this code block when Gemini rate limit is increased 
+            if i % 4 == 0 :
+                time.sleep(1)
         
     graph.add_graph_documents(graph_document_list)
     return  graph_document_list
