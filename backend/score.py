2d3cb4726d9e61757c44bdaea1a47c6e873aeffb
aashipandya <156318202+aashipandya@users.noreply.github.com>
2024-04-12 12:03:31 +0000

    combining 4 chunks

--- backend/score.py
+++ backend/score.py
@@ -159,7 +159,7 @@ async def extract_knowledge_graph_from_file(
 
         elif source_type == 'youtube' and source_url:
             result = await asyncio.to_thread(
-                extract_graph_from_file_youtube, uri, userName, password, model, databast, source_url)
+                extract_graph_from_file_youtube, uri, userName, password, model, database, source_url)
 
         elif source_type == 'Wikipedia' and wiki_query:
             result = await asyncio.to_thread(
@@ -169,9 +169,6 @@ async def extract_knowledge_graph_from_file(
             result = await asyncio.to_thread(
                 extract_graph_from_file_gcs, uri, userName, password, model, database, 
                 gcs_bucket_name, gcs_bucket_folder, gcs_blob_filename)
-
-        else:
-            return {"status": "Failed", "error": "No file found"}
         
         return create_api_response('Success',data=result)
     except Exception as e:
--- backend/src/shared/common_fn.py
+++ backend/src/shared/common_fn.py
@@ -1,5 +1,6 @@
 import logging
 from src.document_sources.youtube import create_youtube_url
+from langchain.docstore.document import Document
 import re
 
 def check_url_source(url):
@@ -29,14 +30,27 @@ def check_url_source(url):
       logging.error(f"Error in recognize URL: {e}")  
       raise Exception(e)
 
+def get_combined_chunks(lst_chunks):
+    combined_chunk_document_list=[]
+    combined_chunks_page_content = ["".join(document['chunk_doc'].page_content for document in lst_chunks[i:i+4]) for i in range(0, len(lst_chunks),4)]
+    combined_chunks_ids = [[document['chunk_id'] for document in lst_chunks[i:i+4]] for i in range(0, len(lst_chunks),4)]
+    
+    for i in range(len(combined_chunks_page_content)):
+         combined_chunk_document_list.append(Document(page_content=combined_chunks_page_content[i], metadata={"combined_chunk_ids":combined_chunks_ids[i]}))
+    return combined_chunk_document_list
+
 
 def get_chunk_and_graphDocument(graph_document_list, lst_chunks):
   logging.info("creating list of chunks and graph documents in get_chunk_and_graphDocument func")
   lst_chunk_chunkId_document=[]
   for graph_document in graph_document_list:
-            for index, chunk in enumerate(lst_chunks):
-                if graph_document.source.page_content == chunk['chunk_doc'].page_content:
-                    position = index+1
-                    lst_chunk_chunkId_document.append({'position':position,'graph_doc':graph_document,'chunk_id':chunk['chunk_id']})
-                    break 
-  return lst_chunk_chunkId_document                   
+            # for index, chunk in enumerate(lst_chunks):
+            #     if graph_document.source.page_content == chunk['chunk_doc'].page_content:
+            #         position = index+1
+            #         lst_chunk_chunkId_document.append({'position':position,'graph_doc':graph_document,'chunk_id':chunk['chunk_id']})
+            #         break 
+          for chunk_id in graph_document.source.metadata['combined_chunk_ids'] :
+            lst_chunk_chunkId_document.append({'graph_doc':graph_document,'chunk_id':chunk_id})
+                  
+  return lst_chunk_chunkId_document  
+                 
