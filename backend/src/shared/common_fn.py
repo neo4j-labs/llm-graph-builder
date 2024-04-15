e67c79dbfec94696469a865c8e6ac7108f4d2e07
vasanthasaikalluri <165021735+vasanthasaikalluri@users.noreply.github.com>
2024-04-15 14:04:17 +0000

    Added Common function

--- backend/src/shared/common_fn.py
+++ backend/src/shared/common_fn.py
@@ -1,5 +1,8 @@
 import logging
 from src.document_sources.youtube import create_youtube_url
+from langchain_community.embeddings.sentence_transformer import SentenceTransformerEmbeddings
+from langchain_google_vertexai import VertexAIEmbeddings
+from langchain_openai import OpenAIEmbeddings
 from langchain.docstore.document import Document
 import re
 import os
@@ -56,4 +59,24 @@ def get_chunk_and_graphDocument(graph_document_list, chunkId_chunkDoc_list):
             lst_chunk_chunkId_document.append({'graph_doc':graph_document,'chunk_id':chunk_id})
                   
   return lst_chunk_chunkId_document  
+
+
+def load_embedding_model(embedding_model_name: str):
+    if embedding_model_name == "openai":
+        embeddings = OpenAIEmbeddings()
+        dimension = 1536
+        logging.info(f"Embedding: Using OpenAI Embeddings , Dimension:{dimension}")
+    elif embedding_model_name == "vertexai":        
+        embeddings = VertexAIEmbeddings(
+            model="textembedding-gecko@003"
+        )
+        dimension = 768
+        logging.info(f"Embedding: Using Vertex AI Embeddings , Dimension:{dimension}")
+    else:
+        embeddings = SentenceTransformerEmbeddings(
+            model_name="all-MiniLM-L6-v2"#, cache_folder="/embedding_model"
+        )
+        dimension = 384
+        logging.info(f"Embedding: Using SentenceTransformer , Dimension:{dimension}")
+    return embeddings, dimension
                  
