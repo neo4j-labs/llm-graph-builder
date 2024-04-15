b8b52b0376045c34dbd9164f0b5b4651febca720
vasanthasaikalluri <165021735+vasanthasaikalluri@users.noreply.github.com>
2024-04-15 11:47:46 +0000

    added multiple models and embeddings for chatbot

--- backend/score.py
+++ backend/score.py
@@ -218,9 +218,11 @@ async def update_similarity_graph(uri=Form(None), userName=Form(None), password=
         return create_api_response(job_status, message=message, error=error_message)
         
 @app.post("/chat_bot")
-async def chat_bot(uri=Form(None), userName=Form(None), password=Form(None), question=Form(None), session_id=Form(None)):
+async def chat_bot(uri=Form(None),model=Form(None),userName=Form(None), password=Form(None), question=Form(None), session_id=Form(None)):
     try:
-        result = await asyncio.to_thread(QA_RAG,uri=uri,userName=userName,password=password,question=question,session_id=session_id)
+        # model=Form(None),
+        # model = "Gemini Pro"
+        result = await asyncio.to_thread(QA_RAG,uri=uri,model=model,userName=userName,password=password,question=question,session_id=session_id)
         return create_api_response('Success',data=result)
     except Exception as e:
         job_status = "Failed"
