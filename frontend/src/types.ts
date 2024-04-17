8041e32f6f40c8d693a8b41ad71793f0e0d8b564
kartikpersistent <101251502+kartikpersistent@users.noreply.github.com>
2024-04-17 08:56:17 +0000

    added sources links in the chat bot message

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -147,8 +147,8 @@ export interface messages {
   message: string;
   user: string;
   datetime: string;
-  isTyping?: boolean
-  sources?:string[]
+  isTyping?: boolean;
+  sources?: string[];
 }
 export type ChatbotProps = {
   messages: messages[];
