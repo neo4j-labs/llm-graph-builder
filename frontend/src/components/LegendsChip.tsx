b0669c74ad89c5d113dc90e33d2dccbc4ce4e4a3
Prakriti Solankey <prakriti_solankey@persistent.com>
2024-04-23 16:14:04 +0530

    'color'

--- frontend/src/components/LegendsChip.tsx
+++ frontend/src/components/LegendsChip.tsx
@@ -1,9 +1,7 @@
 import { useMemo } from 'react';
 import { LegendChipProps } from '../types';
-import { colors } from '../utils/Constants';
 
 export const LegendsChip: React.FunctionComponent<LegendChipProps> = ({ scheme, title, nodes }) => {
-  const showCount = useMemo(() => Object.keys(scheme).length <= colors.length, []);
   const chunkcount = useMemo(
     // @ts-ignore
     () => [...new Set(nodes?.filter((n) => n?.labels?.includes(title)).map((i) => i.caption))].length,
@@ -12,7 +10,7 @@ export const LegendsChip: React.FunctionComponent<LegendChipProps> = ({ scheme, 
   return (
     <div className='legend' key={scheme.key} style={{ backgroundColor: `${scheme[title]}` }}>
       {title}
-      {showCount && chunkcount && `(${chunkcount})`}
+      {chunkcount && `(${chunkcount})`}
     </div>
   );
 };
