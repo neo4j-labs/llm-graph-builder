d6e053a21f3b125d9830280a3afde735e7d88fab
Prakriti Solankey <156313631+prakriti-solankey@users.noreply.github.com>
2024-04-26 21:12:57 +0530

    'hoverchanges' (#254)

--- frontend/src/components/FileTable.tsx
+++ frontend/src/components/FileTable.tsx
@@ -42,8 +42,8 @@ const FileTable: React.FC<FileTableProps> = ({ isExpanded, connectionStatus, set
                   info.row.original?.fileSource === 's3 bucket'
                     ? info.row.original?.source_url
                     : info.row.original?.fileSource === 'youtube'
-                    ? info.row.original?.source_url
-                    : info.getValue()
+                      ? info.row.original?.source_url
+                      : info.getValue()
                 }
               >
                 {info.getValue()}
@@ -57,13 +57,9 @@ const FileTable: React.FC<FileTableProps> = ({ isExpanded, connectionStatus, set
       columnHelper.accessor((row) => row.status, {
         id: 'status',
         cell: (info) => (
-          <div>
+          <div className='cellClass' title={info.row.original?.status === 'Failed' ? info.row.original?.errorMessage : ''}>
             <StatusIndicator type={statusCheck(info.getValue())} />
-            {info.row.original?.status === 'Failed' ? (
-              <span title={info.row.original?.errorMessage}>{info.getValue()}</span>
-            ) : (
-              <i>{info.getValue()}</i>
-            )}
+            <i>{info.getValue()}</i>
           </div>
         ),
         header: () => <span>Status</span>,
@@ -198,14 +194,14 @@ const FileTable: React.FC<FileTableProps> = ({ isExpanded, connectionStatus, set
                     item.fileSource === 's3 bucket' && localStorage.getItem('accesskey') === item?.awsAccessKeyId
                       ? item.status
                       : item.fileSource === 'local file'
-                      ? item.status
-                      : item.status === 'Completed' || item.status === 'Failed'
-                      ? item.status
-                      : item.fileSource == 'Wikipedia' ||
-                        item.fileSource == 'youtube' ||
-                        item.fileSource == 'gcs bucket'
-                      ? item.status
-                      : 'N/A',
+                        ? item.status
+                        : item.status === 'Completed' || item.status === 'Failed'
+                          ? item.status
+                          : item.fileSource == 'Wikipedia' ||
+                            item.fileSource == 'youtube' ||
+                            item.fileSource == 'gcs bucket'
+                            ? item.status
+                            : 'N/A',
                   model: item?.model ?? model,
                   id: uuidv4(),
                   source_url: item.url != 'None' && item?.url != '' ? item.url : '',
