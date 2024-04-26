cf181a8e546241fa84f8d0dc6a0d24f609181484
kartikpersistent <101251502+kartikpersistent@users.noreply.github.com>
2024-04-26 06:05:03 +0000

    fixed chatbot aspect ratio/width issue

--- frontend/src/components/Chatbot.tsx
+++ frontend/src/components/Chatbot.tsx
@@ -112,7 +112,7 @@ export default function Chatbot(props: ChatbotProps) {
   }, [listMessages]);
 
   return (
-    <div className='n-bg-palette-neutral-bg-weak flex flex-col justify-between min-h-full max-h-full overflow-hidden w-[312px]'>
+    <div className='n-bg-palette-neutral-bg-weak flex flex-col justify-between min-h-full max-h-full overflow-hidden'>
       <div className='flex overflow-y-auto pb-12 min-w-full chatBotContainer'>
         <Widget className='n-bg-palette-neutral-bg-weak' header='' isElevated={false}>
           <div className='flex flex-col gap-4 gap-y-4'>
