7c5ff9256c893672f396a92dd61aaf9b5ee79eec
Pravesh Kumar <121786590+praveshkumar1988@users.noreply.github.com>
2024-05-01 10:20:11 +0000

    Code refatctor test cases

--- backend/test_integrationqa.py
+++ backend/test_integrationqa.py
@@ -1,341 +1,185 @@
+from score import *
 from src.main import *
-from fastapi import UploadFile, File
-from tempfile import NamedTemporaryFile
 import logging
-import shutil
-from langchain_community.document_loaders import WikipediaLoader
-from src.api_response import create_api_response
-from langchain_community.document_loaders import S3DirectoryLoader
-import boto3
-import os
-from urllib.parse import urlparse
 from src.QA_integration import QA_RAG
+import asyncio
 
 uri =''
 userName =''
 password =''
 model ='OpenAI GPT 3.5'
 database =''
-gcs_bucket_name = ''
-gcs_bucket_folder = ''
-gcs_blob_filename = ''
-source_url = ''
-aws_access_key_id = ''
-aws_secret_access_key = ''
-
 
 graph = create_graph_database_connection(uri, userName, password, database)
 
-def extract_graph_from_file_local_file_test():
-    """
-    Test function to extract graph from a local file.
+def test_graph_from_file_local_file():
+    file_name = 'Copy01_patrick_pichette_-_wikipedia.pdf'
+    shutil.copyfile('data/Copy01_patrick_pichette_-_wikipedia.pdf', 'backend/src/merged_files/Copy01_patrick_pichette_-_wikipedia.pdf')
+    obj_source_node = sourceNode()
+    obj_source_node.file_name = file_name
+    obj_source_node.file_type = 'pdf'
+    obj_source_node.file_size = '1087'
+    obj_source_node.file_source = 'local file'
+    obj_source_node.model = model
+    obj_source_node.created_at = datetime.now()
+    graphDb_data_Access = graphDBdataAccess(graph)
+    graphDb_data_Access.create_source_node(obj_source_node)
 
-    This function copies a PDF file from a source directory to a destination directory,
-    then calls the 'extract_graph_from_file_local_file' function to extract the graph 
-    from the copied PDF file. It then asserts the result of the extraction process.
+    local_file_result =  extract_graph_from_file_local_file(graph, model, file_name, '', '')
 
-    Returns:
-        None
-    """
-
-    try:
-        # Copy PDF file from source to destination directory
-        shutil.copyfile('data/Copy01_patrick_pichette_-_wikipedia.pdf', 'backend/src/merged_files/Copy01_patrick_pichette_-_wikipedia.pdf')
-        
-        # Call 'extract_graph_from_file_local_file' function
-        local_file_result =  extract_graph_from_file_local_file(
-            graph,
-            model,
-            "Copy01_patrick_pichette_-_wikipedia.pdf"
-        )
-
-        # Print the result of the extraction process
-        print(local_file_result)
-
-        # Assert the result of the extraction process
-        assert local_file_result['status'] == 'Completed' and local_file_result['nodeCount']>30 and local_file_result['relationshipCount']>20
-        print("Success")
-    except FileNotFoundError:
-        print("Fail: File not found.")
-    except Exception as e:
-        print("Fail:", e)
-        logging.error(f"Error occurred: {e}")
-
-
-def extract_graph_from_Wikipedia(uri, userName, password, model, database):
-    """
-    Extracts graph data from Wikipedia.
-
-    This function creates a source node for Wikipedia in the graph, extracts graph data
-    from a specified Wikipedia page, and then asserts the result of the extraction process.
-
-    Args:
-        uri (str): The URI of the Wikipedia page.
-        userName (str): The username for authentication.
-        password (str): The password for authentication.
-        model: The graph model.
-        database: The database connection.
-
-    Returns:
-        None
-    """
-
-    try:
-        # Create source node for Wikipedia in the graph
-        create_source_node_graph_url_wikipedia(graph, model, 'Norway', 'Wikipedia')
-        
-        # Extract graph data from the specified Wikipedia page
-        wikiresult = extract_graph_from_file_Wikipedia(graph, model, 'Norway', 1, '', '')
-
-        # Print the result of the extraction process
-        print(wikiresult)
-
-        # Assert the result of the extraction process
-        assert wikiresult['status'] == 'Completed' and wikiresult['nodeCount'] > 5 and wikiresult['relationshipCount'] > 3
-        print("Success")
-    except FileNotFoundError:
-        print("Fail: File not found.")
-    except Exception as e:
-        print("Fail:", e)
-        logging.error(f"Error occurred: {e}")
-
-    #   Check for Wikipedia file to be Failed
-def get_documents_from_Wikipedia(wiki_query: str):
-    """
-    Retrieves documents from Wikipedia based on the provided query.
-
-    This function retrieves documents from Wikipedia using the provided query.
-    It logs the total number of pages retrieved and asserts True if the retrieval
-    process is successful.
-
-    Args:
-        wiki_query (str): The query string to search for documents on Wikipedia.
-
-    Returns:
-        None
-    """
-    file_name = ''
+    print(local_file_result)
     
+    logging.info("Info:  ")
     try:
-        # Retrieve documents from Wikipedia based on the query
-        pages = WikipediaLoader(query=wiki_query.strip(), load_max_docs=1, load_all_available_meta=False).load()
-        file_name = wiki_query.strip()
-        logging.info(f"Total Pages from Wikipedia = {len(pages)}")
-        print(file_name, pages)
-        
-        # Assert True if retrieval process is successful
-        assert True
-    except Exception as e:
-        # Handle any exceptions that occur during the retrieval process
-        job_status = "Failed"
-        message = "Failed To Process Wikipedia Query"
-        error_message = str(e)
-        logging.error(f"An error occurred while processing Wikipedia query: {error_message}")
-        print(error_message)
-
-def extract_graph_from_youtube_video(uri, userName, password, model, database):
-    """
-    Extracts graph data from a YouTube video.
-
-    This function creates a source node for the YouTube video in the graph, extracts
-    graph data from the specified YouTube video, and then asserts the result of the
-    extraction process.
-
-    Args:
-        uri (str): The URI of the YouTube video.
-        userName (str): The username for authentication.
-        password (str): The password for authentication.
-        model: The graph model.
-        database: The database connection.
-
-    Returns:
-        None
-    """
-
-    try:
-        # Create source node for the YouTube video in the graph
-        create_source_node_graph_url_youtube(graph, model, 'https://www.youtube.com/watch?v=T-qy-zPWgqA', 'youtube')
-        
-        # Extract graph data from the specified YouTube video
-        youtuberesult = extract_graph_from_file_youtube(graph, model, 'https://www.youtube.com/watch?v=T-qy-zPWgqA', '', '')
-
-        # Print the result of the extraction process
-        print(youtuberesult)
-
-        # Assert the result of the extraction process
-        assert youtuberesult['status'] == 'Completed' and youtuberesult['nodeCount'] > 60 and youtuberesult['relationshipCount'] > 50
+        assert local_file_result['status'] == 'Completed' and local_file_result['nodeCount']>3 and local_file_result['relationshipCount']>2
         print("Success")
-    except FileNotFoundError:
-        print("Failed: File not found.")
-    except Exception as e:
-        print("Failed:", e)
-        logging.error(f"Error occurred: {e}")
+    except AssertionError as e:
+        print("Fail: ", e)
 
+def test_graph_from_file_local_file_failed():
+    file_name = 'Not_exist.pdf'
+    try:
+        obj_source_node = sourceNode()
+        obj_source_node.file_name = file_name
+        obj_source_node.file_type = 'pdf'
+        obj_source_node.file_size = '0'
+        obj_source_node.file_source = 'local file'
+        obj_source_node.model = model
+        obj_source_node.created_at = datetime.now()
+        graphDb_data_Access = graphDBdataAccess(graph)
+        graphDb_data_Access.create_source_node(obj_source_node)
+
+        local_file_result =  extract_graph_from_file_local_file(graph, model, file_name, '', '')
+
+        print(local_file_result)
+    except AssertionError as e:
+        print('Failed due to file does not exist means not uploaded or accidentaly deleteled from server')
+        print("Failed: Error from extract function ", e)
+
+#   Check for Wikipedia file to be success
+def test_graph_from_Wikipedia():
+    wiki_query = 'Norway'
+    source_type = 'Wikipedia'
+    create_source_node_graph_url_wikipedia(graph, model, wiki_query, source_type)
+    wikiresult = extract_graph_from_file_Wikipedia(graph, model, wiki_query, 1, '', '')    
+    logging.info("Info: Wikipedia test done")
+    print()
+    try:
+        assert wikiresult['status'] == 'Completed' and wikiresult['nodeCount']>5 and wikiresult['relationshipCount']>3
+        print("Success")
+    except AssertionError as e:
+        print("Fail ", e)
+
+def test_graph_from_Wikipedia_failed():
+    wiki_query = 'Test QA 123456'
+    source_type = 'Wikipedia'
+    try:
+        logging.info("Created source node for wikipedia")
+        create_source_node_graph_url_wikipedia(graph, model, wiki_query, source_type)
+    except AssertionError as e:
+        print("Fail ", e)
+
+
+# Check for Youtube_video to be Success
+def test_graph_from_youtube_video():
+    url = 'https://www.youtube.com/watch?v=T-qy-zPWgqA'
+    source_type = 'youtube'
+
+    create_source_node_graph_url_youtube(graph, model,url , source_type)
+    youtuberesult = extract_graph_from_file_youtube(graph, model, url, '', '')
+
+    logging.info("Info: Youtube Video test done")
+    print(youtuberesult)
+    try:
+        assert youtuberesult['status'] == 'Completed' and youtuberesult['nodeCount']>60 and youtuberesult['relationshipCount']>50
+        print("Success")
+    except AssertionError as e:
+        print("Failed ", e)
+    
 # Check for Youtube_video to be Failed
 
-# def extract_graph_from_youtube_video_fail(uri, userName, password, model, database):
-#     youtuberesults =  extract_graph_from_file_youtube(
-#              uri,
-#             userName,
-#             password,
-#             model,
-#             database,
-#             'https://www.youtube.com/watch?v=U9mJuUkhUzk'
-#             )
-#     # print(result)
-#     print(youtuberesults)
-#     try:
-#         assert youtuberesults['status'] == 'Completed'
-#         print("Success")
-#     except AssertionError as e:
-#         print("Failed ", e)
- 
-def extract_graph_from_file_test_gcs(uri, userName, password, model, database):
-    """
-    Test function to extract graph from a file stored in Google Cloud Storage (GCS).
+def test_graph_from_youtube_video_failed():
+    url = 'https://www.youtube.com/watch?v=U9mJuUkhUzk'
+    source_type = 'youtube'
 
-    This function creates a source node for a file stored in GCS in the graph, extracts
-    graph data from the specified file, and then asserts the result of the extraction process.
-
-    Args:
-        uri (str): The URI of the GCS bucket.
-        userName (str): The username for authentication.
-        password (str): The password for authentication.
-        model: The graph model.
-        database: The database connection.
-
-    Returns:
-        None
-    """
-
+    create_source_node_graph_url_youtube(graph, model,url , source_type)
+    youtuberesult = extract_graph_from_file_youtube(graph, model, url, '', '')
+    # print(result)
+    print(youtuberesult)
     try:
-        # Create source node for the file stored in GCS in the graph
-        create_source_node_graph_url_gcs(graph, model, 'llm_graph_transformer_test', 'technology', 'gcs bucket')
-        
-        # Extract graph data from the specified file stored in GCS
-        gcsresult = extract_graph_from_file_gcs(graph, model, 'llm_graph_transformer_test', 'technology', 'Neuralink brain chip patient playing chess.pdf', '', '')
-
-        # Print the result of the extraction process
-        print(gcsresult)
-
-        # Assert the result of the extraction process
-        assert gcsresult['status'] == 'Completed' and gcsresult['nodeCount'] > 10 and gcsresult['relationshipCount'] > 5
+        assert youtuberesult['status'] == 'Completed'
         print("Success")
-    except FileNotFoundError:
-        print("Failed: File not found.")
-    except Exception as e:
-        print("Failed:", e)
-        logging.error(f"Error occurred: {e}")
+    except AssertionError as e:
+        print("Failed ", e)
+    
+# Check for the GCS file to be uploaded, process and completed
 
-def extract_graph_from_file_test_s3(uri, userName, password, model, database):
-    """
-    Test function to extract graph from a file stored in Amazon S3.
+def test_graph_from_file_test_gcs():
 
-    This function creates a source node for a file stored in Amazon S3 in the graph, 
-    extracts graph data from the specified file, and then asserts the result of the 
-    extraction process.
-
-    Args:
-        uri (str): The URI of the S3 bucket.
-        userName (str): The AWS access key ID.
-        password (str): The AWS secret access key.
-        model: The graph model.
-        database: The database connection.
-
-    Returns:
-        None
-    """
-    try:
-        # Create source node for the file stored in S3 in the graph
-        create_source_node_graph_url_s3(graph, model, 's3://development-llm-graph-builder-models/data/Sundar_Pichai_Gemini.pdf', '', '', 's3 bucket')
-
-        # Not passing key here
-        # Extract graph data from the specified file stored in S3
-
-        s3result = extract_graph_from_file_s3(graph, model, 's3://development-llm-graph-builder-models/data/Sundar_Pichai_Gemini.pdf', '', '', '', '')
-
-        # Assert the result of the extraction process
-        assert s3result['status'] == 'Completed' and s3result['nodeCount'] > 5 and s3result['relationshipCount'] > 10
-        print("Success")
-    except FileNotFoundError:
-        print("Fail: File not found.")
-    except Exception as e:
-        print("Fail:", e)
-        logging.error(f"Error occurred: {e}")
-
-    # Print Result
+    bucket_name = 'llm_graph_transformer_test'
+    folder_name = 'technology'
+    source_type ='gcs bucket'
+    file_name = 'Neuralink brain chip patient playing chess.pdf'
+    create_source_node_graph_url_gcs(graph, model, bucket_name, folder_name, source_type)
+    gcsresult = extract_graph_from_file_gcs(graph, model, bucket_name, folder_name, file_name, '', '')
+    
     logging.info("Info")
-    print(s3result)  
-
-
-    ####
-# def get_documents_from_s3(s3_url, aws_access_key_id, aws_secret_access_key):
-#     try:
-#       parsed_url = urlparse(s3_url)
-#       bucket = parsed_url.netloc
-#       file_key = parsed_url.path.lstrip('/')
-#       file_name=file_key.split('/')[-1]
-#       s3=boto3.client('s3',aws_access_key_id=aws_access_key_id,aws_secret_access_key=aws_secret_access_key)
-#       response=s3.head_object(Bucket=bucket,Key=file_key)
-#       file_size=response['ContentLength']
-      
-#       logging.info(f'bucket : {bucket},file_name:{file_name},  file key : {file_key},  file size : {file_size}')
-#       pages=get_s3_pdf_content(s3_url,aws_access_key_id=aws_access_key_id,aws_secret_access_key=aws_secret_access_key)
-#       return file_name,pages
-#     except Exception as e:
-#       error_message = str(e)
-#       logging.exception(f'Exception in reading content from S3:{error_message}')
-#       raise Exception(error_message) 
-
-def chatbot_QnA(uri, userName, password, model, database):
-    """
-    Performs a question-answer operation using a chatbot.
-
-    This function queries a chatbot with a question and retrieves the answer.
-    It then asserts the length of the answer message to ensure it meets a minimum length requirement.
-
-    Args:
-        uri (str): The URI for the chatbot service.
-        userName (str): The username for authentication.
-        password (str): The password for authentication.
-        model: The chatbot model.
-        database: The database connection.
-
-    Returns:
-        None
-    """
-
+    print(gcsresult)
+    
     try:
-        # Perform question-answer operation using the chatbot
-        QA_n_RAG = QA_RAG(
-            uri,
-            model,
-            userName,
-            password,
-            'who is patrick pichette',
-            1
-        )
-        
-        # Print the result of the question-answer operation
-        print(QA_n_RAG)
-        print(len(QA_n_RAG['message']))
+        assert gcsresult['status'] == 'Completed' and gcsresult['nodeCount']>10 and gcsresult['relationshipCount']>5
+        print("Success")
+    except AssertionError as e:
+        print("Failed ", e)
 
-        # Assert the length of the answer message
+def test_graph_from_file_test_gcs_failed():
+
+    bucket_name = 'llm_graph_transformer_neo'
+    folder_name = 'technology'
+    source_type ='gcs bucket'
+    # file_name = 'Neuralink brain chip patient playing chess.pdf'
+    try:
+        create_source_node_graph_url_gcs(graph, model, bucket_name, folder_name, source_type)
+        print("GCS: Create source node failed due to bucket not exist")
+    except AssertionError as e:
+        print("Failed ", e)
+
+def test_graph_from_file_test_s3_failed():
+    source_url = 's3://development-llm-graph-builder-models/'
+    try:
+        create_source_node_graph_url_s3(graph,model,source_url,'test123','pwd123')
+        # assert result['status'] == 'Failed'
+        # print("S3 created source node failed die to wrong access key id and secret")
+    except AssertionError as e:
+        print("Failed ", e)
+
+# Check the Functionality of Chatbot QnA
+def test_chatbot_QnA():
+    QA_n_RAG = QA_RAG(uri, model, userName, password, 'who is patrick pichette',1 )
+    
+    print(QA_n_RAG)
+    print(len(QA_n_RAG['message']))
+    try:
         assert len(QA_n_RAG['message']) > 10
         print("Success")
-    except FileNotFoundError:
-        print("Failed: File not found.")
-    except Exception as e:
-        print("Failed:", e)
-        logging.error(f"Error occurred: {e}")
+    except AssertionError as e:
+        print("Failed ", e)
 
 
 if __name__ == "__main__":
 
-        extract_graph_from_file_local_file_test()
-        extract_graph_from_Wikipedia(uri, userName, password, model,database)
-        get_documents_from_Wikipedia("  ")
-        extract_graph_from_youtube_video(uri, userName, password, model, database)
-        extract_graph_from_file_test_gcs(uri, userName, password, model, database)
-        extract_graph_from_file_test_s3(uri, userName, password, model, database)
-        chatbot_QnA(uri, userName, password, model, database)
+    # test_graph_from_file_local_file() # local file Success Test Case
+    # test_graph_from_file_local_file_failed() # local file Failed Test Case
+
+    test_graph_from_Wikipedia() # Wikipedia Success Test Case
+    # test_graph_from_Wikipedia_failed() # Wikipedia Failed Test Case
+
+    # test_graph_from_youtube_video() # Youtube Success Test Case
+    # test_graph_from_youtube_video_failed # Failed Test case
+
+    # test_graph_from_file_test_gcs() # GCS Success Test Case
+    # test_graph_from_file_test_gcs_failed() # GCS Failed Test Case
+
+    # test_graph_from_file_test_s3_failed() # S3 Failed Test Case
+    
+    # test_chatbot_QnA(uri, userName, password, model, database)
