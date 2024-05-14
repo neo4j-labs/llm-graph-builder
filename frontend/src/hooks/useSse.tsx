8a7b336c21e738e1b9af7da43ec67e1ca27af973
kartikpersistent <101251502+kartikpersistent@users.noreply.github.com>
2024-05-14 21:04:18 +0530

    Implemented polling for status update (#309)
    * Implemented polling for status update
    
    * status updation for large files
    
    * added example env in the frontend
    
    * updated the readme with frontend env info
    
    * readme changes
    
    * readme updates
    
    * setting up failed status

---
+++ frontend/src/hooks/useSse.tsx
@@ -0,0 +1,55 @@
+import { useFileContext } from '../context/UsersFiles';
+import { eventResponsetypes } from '../types';
+const perchunksecond = parseInt(process.env.TIME_PER_CHUNK as string);
+export default function useServerSideEvent(
+  alertHandler: (minutes: number, filename: string) => void,
+  errorHandler: (filename: string) => void
+) {
+  const { setFilesData } = useFileContext();
+  function updateStatusForLargeFiles(eventSourceRes: eventResponsetypes) {
+    const { fileName, nodeCount, processingTime, relationshipCount, status, total_chunks, model } = eventSourceRes;
+    const alertShownStatus = JSON.parse(localStorage.getItem('alertShown') || 'null');
+    if (status === 'Processing' && alertShownStatus != null && alertShownStatus == false && total_chunks != null) {
+      const minutes = Math.floor((perchunksecond * total_chunks) / 60);
+      alertHandler(minutes, fileName);
+    } else if (status === 'Completed') {
+      setFilesData((prevfiles) => {
+        return prevfiles.map((curfile) => {
+          if (curfile.name == fileName) {
+            return {
+              ...curfile,
+              status: status,
+              NodesCount: nodeCount,
+              relationshipCount: relationshipCount,
+              model: model,
+              processing: processingTime?.toFixed(2),
+            };
+          }
+          return curfile;
+        });
+      });
+    } else if (eventSourceRes.status === 'Failed') {
+      setFilesData((prevfiles) => {
+        return prevfiles.map((curfile) => {
+          if (curfile.name == fileName) {
+            return {
+              ...curfile,
+              status: status,
+            };
+          }
+          return curfile;
+        });
+      });
+      errorHandler(fileName);
+    }
+  }
+
+  const serverSideErrorHandler = (filename: string) => {
+    errorHandler(filename);
+  };
+
+  return {
+    updateStatusForLargeFiles,
+    serverSideErrorHandler,
+  };
+}
