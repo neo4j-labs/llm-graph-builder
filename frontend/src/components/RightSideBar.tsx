b5a9911c4cb0762d2ff81d516dc11d9cf65bb73f
Prakriti Solankey <156313631+prakriti-solankey@users.noreply.github.com>
2024-05-16 17:56:19 +0000

    expand

--- frontend/src/components/RightSideBar.tsx
+++ frontend/src/components/RightSideBar.tsx
@@ -3,7 +3,7 @@ import Chatbot from './Chatbot';
 import chatbotmessages from '../assets/ChatbotMessages.json';
 import { useState } from 'react';
 import { UserCredentials, Messages } from '../types';
-import { ArchiveBoxXMarkIconOutline } from '@neo4j-ndl/react/icons';
+import { ExpandIcon, ShrinkIcon, TrashIconOutline } from '@neo4j-ndl/react/icons';
 import ButtonWithToolTip from './ButtonWithToolTip';
 import { clearChatAPI } from '../services/QnaAPI';
 import { useCredentials } from '../context/UserCredentials';
@@ -12,45 +12,57 @@ interface RightSideBarProps {
   showChatBot: boolean;
   closeChatBot: () => void;
 }
-
 const RightSideBar: React.FC<RightSideBarProps> = ({ showChatBot, closeChatBot }) => {
   const date = new Date();
+  const formattedDateTime = `${date.toLocaleDateString()} ${date.toLocaleTimeString()}`;
   const [messages, setMessages] = useState<Messages[]>([
-    { ...chatbotmessages.listMessages[1], datetime: `${date.toLocaleDateString()} ${date.toLocaleTimeString()}` },
+    { ...chatbotmessages.listMessages[1], datetime: formattedDateTime },
   ]);
   const [clearHistoryData, setClearHistoryData] = useState<boolean>(false);
   const { userCredentials } = useCredentials();
-
+  const [isFullScreen, setIsFullScreen] = useState<boolean>(false);
   const getIsLoading = (messages: Messages[]) => {
     return messages.some((msg) => msg.isTyping || msg.isLoading);
   };
-
   const deleteOnClick = async () => {
     try {
       setClearHistoryData(true);
-      const response = await clearChatAPI(
-        userCredentials as UserCredentials,
-        sessionStorage.getItem('session_id') ?? ''
-      );
-      console.log('res', response);
+      const response = await clearChatAPI(userCredentials as UserCredentials, sessionStorage.getItem('session_id') ?? '');
       if (response.data.status === 'Success') {
         setMessages([
           {
-            datetime: `${date.toLocaleDateString()} ${date.toLocaleTimeString()}`,
+            datetime: formattedDateTime,
             id: 2,
-            message:
-              ' Welcome to the Neo4j Knowledge Graph Chat. You can ask questions related to documents which have been completely processed.',
+            message: ' Welcome to the Neo4j Knowledge Graph Chat. You can ask questions related to documents which have been completely processed.',
             sources: ['https://neo4j.com/'],
             user: 'chatbot',
           },
         ]);
-        setClearHistoryData(false);
       }
     } catch (error) {
       console.log(error);
+    } finally {
       setClearHistoryData(false);
     }
   };
+
+  const toggleFullscreen = () => {
+    setIsFullScreen(!isFullScreen);
+    alert('hello')
+  };
+
+  // const toggleFullscreen = () => {
+  //   setIsFullScreen(!isFullScreen);
+  //   const chatbotWindow = window.open(
+  //     '',
+  //     '_blank',
+  //     'width=600,height=600,top=100,left=100'
+  //   );
+  //   if (chatbotWindow) {
+  //     chatbotWindow.document.write(``);
+  //     chatbotWindow.document.close();
+  //   }
+  // };
 
   return (
     <Drawer
@@ -59,24 +71,39 @@ const RightSideBar: React.FC<RightSideBarProps> = ({ showChatBot, closeChatBot }
       type='push'
       closeable={true}
       position='right'
-      onExpandedChange={function Ha(expanded) {
+      onExpandedChange={(expanded) => {
         if (!expanded) {
           closeChatBot();
         }
       }}
-      key={'rightdrawer'}
+      key='rightdrawer'
       className='grow'
     >
-      <ButtonWithToolTip
-        text='Clear Chat history'
-        onClick={deleteOnClick}
-        size='medium'
-        clean
-        placement='right'
-        disabled={messages.length === 1}
-      >
-        <ArchiveBoxXMarkIconOutline className='n-size-token-7' style={{ padding: '2px' }} />
-      </ButtonWithToolTip>
+      <div className='inline-flex gap-x-1' style={{ display: 'flex', flexGrow: 0, alignItems: 'center', gap: '6px' }}>
+        <ButtonWithToolTip
+          text='Clear Chat history'
+          onClick={deleteOnClick}
+          size='medium'
+          clean
+          placement='bottom'
+          disabled={messages.length === 1}
+        >
+          <TrashIconOutline className='n-size-token-7' style={{ padding: '2px' }} />
+        </ButtonWithToolTip>
+        <ButtonWithToolTip
+          text='Toggle minimize frame'
+          onClick={toggleFullscreen}
+          size='medium'
+          clean
+          placement='right'
+        >
+          {isFullScreen ? (
+            <ShrinkIcon className="text-palette-neutral-text-weak h-5 w-5" />
+          ) : (
+            <ExpandIcon className="text-palette-neutral-text-weak h-5 w-5" />
+          )}
+        </ButtonWithToolTip>
+      </div>
       <Drawer.Body className='!overflow-y-hidden !px-0'>
         {clearHistoryData ? (
           <LoadingSpinner size='large' className='top-72 left-52' />
@@ -86,7 +113,7 @@ const RightSideBar: React.FC<RightSideBarProps> = ({ showChatBot, closeChatBot }
             setMessages={setMessages}
             clear={clearHistoryData}
             isLoading={getIsLoading(messages)}
-          ></Chatbot>
+          />
         )}
       </Drawer.Body>
     </Drawer>
