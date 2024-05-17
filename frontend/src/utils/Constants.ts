e8ec22c89acf2365c852a3d47e5fca21065b6c59
kartikpersistent <101251502+kartikpersistent@users.noreply.github.com>
2024-05-17 09:47:05 +0000

    OpenAI GPT 4o model label changes

--- frontend/src/utils/Constants.ts
+++ frontend/src/utils/Constants.ts
@@ -28,7 +28,7 @@ export const docChunkEntities = `+[chunks]
 export const llms =
   process.env?.LLM_MODELS?.trim() != ''
     ? process.env.LLM_MODELS?.split(',')
-    : ['Diffbot', 'Gemini 1.0 Pro', 'OpenAI GPT 3.5', 'OpenAI GPT 4', 'Gemini 1.5 Pro'];
+    : ['Diffbot', 'Gemini 1.0 Pro', 'OpenAI GPT 3.5', 'OpenAI GPT 4o', 'Gemini 1.5 Pro'];
 export const defaultLLM = llms?.includes('OpenAI GPT 3.5')
   ? 'OpenAI GPT 3.5'
   : llms?.includes('Gemini 1.0 Pro')
