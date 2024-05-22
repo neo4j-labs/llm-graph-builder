ea970eab6c6af303ef9bf12de48de9eefc4bf195
kartikpersistent <101251502+kartikpersistent@users.noreply.github.com>
2024-05-22 16:39:53 +0530

    321 documents selection for processing and graph visualization (#343)
    * added multi select extraction
    
    * added support for multiple documents
    
    * integrated api
    
    * format fixes
    
    * conditional rendering of limit input
    
    * Graph Query : Added Doc Limit parameter
    
    * showing the selected files length
    
    * handled generating graph using multiselect
    
    * added the count to respective buttons
    
    * removed doclimit
    
    * removed the doc limit
    
    * fixed inspectedname issue
    
    * format fixes
    
    * added toottip message on show graph message
    
    ---------
    
    Co-authored-by: vasanthasaikalluri <165021735+vasanthasaikalluri@users.noreply.github.com>

--- frontend/src/hooks/useSse.tsx
+++ frontend/src/hooks/useSse.tsx
@@ -50,6 +50,5 @@ export default function useServerSideEvent(
 
   return {
     updateStatusForLargeFiles,
-    serverSideErrorHandler,
   };
 }
--- frontend/src/services/PollingAPI.ts
+++ frontend/src/services/PollingAPI.ts
@@ -14,26 +14,29 @@ export default async function subscribe(
 
   const MAX_POLLING_ATTEMPTS = 10;
   let pollingAttempts = 0;
-  let delay = 2000; 
+  let delay = 2000;
 
   while (pollingAttempts < MAX_POLLING_ATTEMPTS) {
+    let currentdelay = delay;
     let response: PollingAPI_Response = await axios.get(
       `${url()}/document_status/${fileName}?url=${uri}&userName=${username}&password=${encodedstr}&database=${database}`
     );
 
     if (response.data?.file_name?.status === 'Processing') {
-      await new Promise((resolve) => setTimeout(resolve, delay));
-      delay *= 2; 
-      pollingAttempts++; 
+      await new Promise((resolve) => {
+        setTimeout(resolve, currentdelay);
+      });
+      delay *= 2;
+      pollingAttempts++;
     } else if (response.status !== 200) {
       throw new Error(
         JSON.stringify({ fileName, message: `Failed To Process ${fileName} or LLM Unable To Parse Content` })
       );
     } else {
       datahandler(response.data);
-      return; 
+      return;
     }
   }
 
-  throw new Error(`Polling for ${fileName} timed out after ${MAX_POLLING_ATTEMPTS} attempts.`); 
+  throw new Error(`Polling for ${fileName} timed out after ${MAX_POLLING_ATTEMPTS} attempts.`);
 }
