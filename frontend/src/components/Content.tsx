fdf9c7ff43dadedcd10cee06f518bdc6611a5f31
Pravesh Kumar <121786590+praveshkumar1988@users.noreply.github.com>
2024-05-23 17:21:13 +0530

    Update graph chunk processed (#358)
    * update graph after fixed number of chunk processed
    
    * update node_count based on no of chunks processed
    
    * Update graph after spefic number of chunks
    
    * removed the large file check
    
    * added missing dependency
    
    ---------
    
    Co-authored-by: kartikpersistent <101251502+kartikpersistent@users.noreply.github.com>

--- frontend/src/components/Content.tsx
+++ frontend/src/components/Content.tsx
@@ -129,7 +129,6 @@ const Content: React.FC<ContentProps> = ({ isExpanded, showChatBot, openChatBot 
         })
       );
 
-      if (fileItem.size != undefined && fileItem.size > 10000000) {
         if (fileItem.name != undefined && userCredentials != null) {
           const name = fileItem.name;
           triggerStatusUpdateAPI(
@@ -141,7 +140,6 @@ const Content: React.FC<ContentProps> = ({ isExpanded, showChatBot, openChatBot 
             updateStatusForLargeFiles
           );
         }
-      }
 
       const apiResponse = await extractAPI(
         fileItem.model,
