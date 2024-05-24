f47e4f3aeb7ef1091d9554ded35d6020a37782a7
Pravesh Kumar <121786590+praveshkumar1988@users.noreply.github.com>
2024-05-24 13:49:01 +0000

    "," included graph view issue fixed

--- backend/src/graph_query.py
+++ backend/src/graph_query.py
@@ -2,6 +2,7 @@ import logging
 from neo4j import time 
 from neo4j import GraphDatabase
 import os
+import json
 # from neo4j.debug import watch
 
 # watch("neo4j")
@@ -249,7 +250,7 @@ def get_graph_results(uri, username, password, query_type,document_names):
     dict: Contains the session ID, user-defined messages with nodes and relationships, and the user module identifier.
     """
     try:
-        logging.info(f"URI: {uri}, Username: {username}, Password: {password}, Query Type: {query_type}, Document Names: {document_names}")
+        # logging.info(f"URI: {uri}, Username: {username}, Password: {password}, Query Type: {query_type}, Document Names: {document_names}")
         logging.info(f"Starting graph query process")
         driver = get_graphDB_driver(uri, username, password)
         # if document_names:
@@ -260,13 +261,14 @@ def get_graph_results(uri, username, password, query_type,document_names):
         #     if len(document_names) > int(doc_limit):
         #         document_names = document_names[:int(doc_limit)]
         #     print(document_names)
-        document_names = document_names.split(",")    
+        # document_names = document_names.split(",")    
         nodes = list()
         relationships = list()
+        document_names= list(map(str.strip, json.loads(document_names)))
         for document in document_names:
             query = get_cypher_query(QUERY_MAP, query_type, document.strip())
             records, summary , keys = execute_query(driver, query, document.strip())
-            print(query)
+            # print(query)
             document_nodes = extract_node_elements(records)
             document_relationships = extract_relationships(records)
             nodes.extend(document_nodes)
