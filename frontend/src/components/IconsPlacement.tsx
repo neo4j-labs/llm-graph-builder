f55707b3168c49f6c27a39085ef21264def14964
Prakriti Solankey <156313631+prakriti-solankey@users.noreply.github.com>
2024-05-28 08:58:03 +0000

    icon tooltip

--- frontend/src/components/IconsPlacement.tsx
+++ frontend/src/components/IconsPlacement.tsx
@@ -1,6 +1,7 @@
 import { TrashIconOutline, XMarkIconOutline } from '@neo4j-ndl/react/icons';
 import { IconButton } from '@neo4j-ndl/react';
 import { Messages } from '../types';
+import ButtonWithToolTip from './ButtonWithToolTip';
 
 interface IconProps {
   closeChatBot: () => void;
@@ -11,9 +12,9 @@ interface IconProps {
 const IconsPlacement: React.FC<IconProps> = ({ closeChatBot, deleteOnClick, messages }) => {
   return (
     <div className='flex items-end justify-end'>
-      <IconButton aria-label='Remove chat history' clean onClick={deleteOnClick} disabled={messages.length === 1}>
+      <ButtonWithToolTip text='Clear chat history' aria-label='Remove chat history' clean onClick={deleteOnClick} disabled={messages.length === 1} placement='left'>
         <TrashIconOutline />
-      </IconButton>
+      </ButtonWithToolTip>
       <IconButton aria-label='Remove chatbot' clean onClick={closeChatBot}>
         <XMarkIconOutline />
       </IconButton>
