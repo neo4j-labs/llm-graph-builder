fae82c32033c41456c2cd9622311493c272ab397
kartikpersistent <101251502+kartikpersistent@users.noreply.github.com>
2024-06-03 06:10:17 +0000

    added the default page size

--- frontend/src/components/FileTable.tsx
+++ frontend/src/components/FileTable.tsx
@@ -118,8 +118,8 @@ const FileTable: React.FC<FileTableProps> = ({ isExpanded, connectionStatus, set
                   info.row.original?.fileSource === 's3 bucket'
                     ? info.row.original?.source_url
                     : info.row.original?.fileSource === 'youtube'
-                    ? info.row.original?.source_url
-                    : info.getValue()
+                      ? info.row.original?.source_url
+                      : info.getValue()
                 }
               >
                 {info.getValue()}
@@ -334,14 +334,14 @@ const FileTable: React.FC<FileTableProps> = ({ isExpanded, connectionStatus, set
                     item.fileSource === 's3 bucket' && localStorage.getItem('accesskey') === item?.awsAccessKeyId
                       ? item.status
                       : item.fileSource === 'local file'
-                      ? item.status
-                      : item.status === 'Completed' || item.status === 'Failed'
-                      ? item.status
-                      : item.fileSource == 'Wikipedia' ||
-                        item.fileSource == 'youtube' ||
-                        item.fileSource == 'gcs bucket'
-                      ? item.status
-                      : 'N/A',
+                        ? item.status
+                        : item.status === 'Completed' || item.status === 'Failed'
+                          ? item.status
+                          : item.fileSource == 'Wikipedia' ||
+                            item.fileSource == 'youtube' ||
+                            item.fileSource == 'gcs bucket'
+                            ? item.status
+                            : 'N/A',
                   model: item?.model ?? model,
                   id: uuidv4(),
                   source_url: item.url != 'None' && item?.url != '' ? item.url : '',
@@ -547,7 +547,7 @@ const FileTable: React.FC<FileTableProps> = ({ isExpanded, connectionStatus, set
     onColumnFiltersChange: setColumnFilters,
     initialState: {
       pagination: {
-        pageSize: pageSizeCalculation,
+        pageSize: pageSizeCalculation < 0 ? 9 : pageSizeCalculation,
       },
     },
     state: {
