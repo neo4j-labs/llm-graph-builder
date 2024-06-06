59a68beb8f2ab54cdc7ea9c12c9c373c20d935a7
kartikpersistent <101251502+kartikpersistent@users.noreply.github.com>
2024-06-06 15:40:55 +0000

    added the json

--- frontend/src/components/SettingModal.tsx
+++ frontend/src/components/SettingModal.tsx
@@ -1,10 +1,11 @@
 import { Button, Dialog, Dropdown } from '@neo4j-ndl/react';
 import { OnChangeValue } from 'react-select';
-import { OptionType, UserCredentials } from '../types';
+import { OptionType, OptionTypeForExamples, UserCredentials } from '../types';
 import { useFileContext } from '../context/UsersFiles';
 import { getNodeLabelsAndRelTypes } from '../services/GetNodeLabelsRelTypes';
 import { useCredentials } from '../context/UserCredentials';
 import { MouseEventHandler, useCallback, useEffect, useState } from 'react';
+import schemaExamples from '../assets/schemas.json'
 
 export default function SettingsModal({ open, onClose }: { open: boolean; onClose: () => void }) {
   const { setSelectedRels, setSelectedNodes, selectedNodes, selectedRels } = useFileContext();
@@ -20,7 +21,22 @@ export default function SettingsModal({ open, onClose }: { open: boolean; onClos
   };
   const [nodeLabelOptions, setnodeLabelOptions] = useState<OptionType[]>([]);
   const [relationshipTypeOptions, setrelationshipTypeOptions] = useState<OptionType[]>([]);
+  const [defaultExamples, setdefaultExamples] = useState<OptionTypeForExamples[]>([]);
 
+  useEffect(() => {
+    const parsedData = schemaExamples.reduce((accu: OptionTypeForExamples[], example) => {
+      const examplevalues: OptionTypeForExamples = {
+        label: example.schema,
+        value: {
+          nodelabels: example.labels,
+          relationshipTypes: example.relationshipTypes
+        }
+      }
+      accu.push(examplevalues)
+      return accu
+    }, [])
+    setdefaultExamples(parsedData)
+  }, [])
   useEffect(() => {
     if (userCredentials && open) {
       const getOptions = async () => {
@@ -54,6 +70,18 @@ export default function SettingsModal({ open, onClose }: { open: boolean; onClos
     <Dialog size='medium' open={open} aria-labelledby='form-dialog-title' onClose={onClose}>
       <Dialog.Header id='form-dialog-title'>Graph Settings</Dialog.Header>
       <Dialog.Content className='n-flex n-flex-col n-gap-token-4'>
+        <Dropdown
+          helpText='Examples For Node an labels'
+          label='Node Labels'
+          selectProps={{
+            isClearable: true,
+            isMulti: true,
+            options: nodeLabelOptions,
+            onChange: onChangenodes,
+            value: selectedNodes,
+          }}
+          type='creatable'
+        />
         <Dropdown
           helpText='You can select more than one values'
           label='Node Labels'
--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -32,6 +32,15 @@ export interface OptionType {
   readonly value: string;
   readonly label: string;
 }
+interface exmapleType{
+  nodelabels:string[],
+  relationshipTypes:string[]
+}
+export interface OptionTypeForExamples{
+  readonly value:exmapleType;
+  readonly label: string;
+
+}
 
 export type UserCredentials = {
   uri: string;
