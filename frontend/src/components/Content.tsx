55ef4a05394f8088cbce95c5a533300775703037
kartikpersistent <101251502+kartikpersistent@users.noreply.github.com>
2024-06-06 07:20:16 +0000

    fixed the layout issue

--- frontend/src/components/Content.tsx
+++ frontend/src/components/Content.tsx
@@ -362,7 +362,7 @@ const Content: React.FC<ContentProps> = ({ isLeftExpanded, isRightExpanded }) =>
       : isRightExpanded
       ? 'contentWithChatBot'
       : !isLeftExpanded && !isRightExpanded
-      ? 'w-100'
+      ? 'w-[calc(100%-128px)]'
       : 'contentWithDropzoneExpansion';
 
   const handleGraphView = () => {
