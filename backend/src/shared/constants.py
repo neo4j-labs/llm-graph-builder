41e5e153f5ba42a666050b1c3e2bde7edcec762f
aashipandya <156318202+aashipandya@users.noreply.github.com>
2024-06-21 10:47:07 +0000

    llm model changes

--- backend/src/shared/constants.py
+++ backend/src/shared/constants.py
@@ -1,17 +1,17 @@
 MODEL_VERSIONS = {
-        "OpenAI GPT 3.5": "gpt-3.5-turbo-16k",
-        "Gemini 1.0 Pro": "gemini-1.0-pro-001",
-        "Gemini 1.5 Pro": "gemini-1.5-pro-preview-0514",
-        "OpenAI GPT 4": "gpt-4-0125-preview",
-        "Diffbot" : "gpt-4o",
-        "OpenAI GPT 4o":"gpt-4o",
-        "Groq llama3" : "llama3-70b-8192"
+        "gpt-3.5": "gpt-3.5-turbo-16k",
+        "gemini-1.0-pro": "gemini-1.0-pro-001",
+        "gemini-1.5-pro": "gemini-1.5-pro-preview-0514",
+        "gpt-4": "gpt-4-0125-preview",
+        "diffbot" : "gpt-4o",
+        "gpt-4o":"gpt-4o",
+        "groq-llama3" : "llama3-70b-8192"
          }
-OPENAI_MODELS = ["OpenAI GPT 3.5", "OpenAI GPT 4o"]
-GEMINI_MODELS = ["Gemini 1.0 Pro", "Gemini 1.5 Pro"]
+OPENAI_MODELS = ["gpt-3.5", "gpt-4o"]
+GEMINI_MODELS = ["gemini-1.0-pro", "gemini-1.5-pro"]
 CHAT_MAX_TOKENS = 1000
 CHAT_SEARCH_KWARG_K = 3
 CHAT_SEARCH_KWARG_SCORE_THRESHOLD = 0.7
-GROQ_MODELS = ["Groq llama3"]
+GROQ_MODELS = ["groq-llama3"]
 BUCKET_UPLOAD = 'llm-graph-builder-upload'
 PROJECT_ID = 'llm-experiments-387609'
