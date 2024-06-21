4451d28b97b82f565e48b2b5ba75ba51863ba946
Prakriti Solankey <156313631+prakriti-solankey@users.noreply.github.com>
2024-06-21 17:04:32 +0530

    File-table no data found (#456)
    * 'file-table''
    
    * review comment

--- frontend/src/components/FileTable.tsx
+++ frontend/src/components/FileTable.tsx
@@ -9,7 +9,7 @@ import {
   TextLink,
   Typography,
 } from '@neo4j-ndl/react';
-import { useEffect, useMemo, useState } from 'react';
+import { useEffect, useMemo, useRef, useState } from 'react';
 import React from 'react';
 import {
   useReactTable,
@@ -46,12 +46,15 @@ const FileTable: React.FC<FileTableProps> = ({ isExpanded, connectionStatus, set
   const columnHelper = createColumnHelper<CustomFile>();
   const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
   const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [currentOuterHeight, setcurrentOuterHeight] = useState<number>(window.outerHeight);
+  //const [currentOuterHeight, setcurrentOuterHeight] = useState<number>(window.outerHeight);
   const [alertDetails, setalertDetails] = useState<alertStateType>({
     showAlert: false,
     alertType: 'error',
     alertMessage: '',
   });
+
+  const tableRef = useRef(null);
+
   const { updateStatusForLargeFiles } = useServerSideEvent(
     (inMinutes, time, fileName) => {
       setalertDetails({
@@ -285,14 +288,14 @@ const FileTable: React.FC<FileTableProps> = ({ isExpanded, connectionStatus, set
                     item?.fileSource === 's3 bucket' && localStorage.getItem('accesskey') === item?.awsAccessKeyId
                       ? item?.status
                       : item?.fileSource === 'local file'
-                      ? item?.status
-                      : item?.status === 'Completed' || item.status === 'Failed'
-                      ? item?.status
-                      : item?.fileSource == 'Wikipedia' ||
-                        item?.fileSource == 'youtube' ||
-                        item?.fileSource == 'gcs bucket'
-                      ? item?.status
-                      : 'N/A',
+                        ? item?.status
+                        : item?.status === 'Completed' || item.status === 'Failed'
+                          ? item?.status
+                          : item?.fileSource == 'Wikipedia' ||
+                            item?.fileSource == 'youtube' ||
+                            item?.fileSource == 'gcs bucket'
+                            ? item?.status
+                            : 'N/A',
                   model: item?.model ?? model,
                   id: uuidv4(),
                   source_url: item?.url != 'None' && item?.url != '' ? item.url : '',
@@ -305,8 +308,8 @@ const FileTable: React.FC<FileTableProps> = ({ isExpanded, connectionStatus, set
                   language: item?.language ?? '',
                   processingProgress:
                     item?.processed_chunk != undefined &&
-                    item?.total_chunks != undefined &&
-                    !isNaN(Math.floor((item?.processed_chunk / item?.total_chunks) * 100))
+                      item?.total_chunks != undefined &&
+                      !isNaN(Math.floor((item?.processed_chunk / item?.total_chunks) * 100))
                       ? Math.floor((item?.processed_chunk / item?.total_chunks) * 100)
                       : undefined,
                   total_pages: item?.total_pages ?? 0,
@@ -385,7 +388,64 @@ const FileTable: React.FC<FileTableProps> = ({ isExpanded, connectionStatus, set
     }
   }, [connectionStatus, userCredentials]);
 
-  function updatestatus(i: statusupdate) {
+  const cancelHandler = async (fileName: string, id: string, fileSource: string) => {
+    setFilesData((prevfiles) =>
+      prevfiles.map((curfile) => {
+        if (curfile.id === id) {
+          return {
+            ...curfile,
+            processingStatus: true,
+          };
+        }
+        return curfile;
+      })
+    );
+    try {
+      const res = await cancelAPI([fileName], [fileSource]);
+      if (res.data.status === 'Success') {
+        setFilesData((prevfiles) =>
+          prevfiles.map((curfile) => {
+            if (curfile.id === id) {
+              return {
+                ...curfile,
+                status: 'Cancelled',
+                processingStatus: false,
+              };
+            }
+            return curfile;
+          })
+        );
+      } else {
+        let errorobj = { error: res.data.error, message: res.data.message, fileName };
+        throw new Error(JSON.stringify(errorobj));
+      }
+    } catch (err) {
+      setFilesData((prevfiles) =>
+        prevfiles.map((curfile) => {
+          if (curfile.id === id) {
+            return {
+              ...curfile,
+              processingStatus: false,
+            };
+          }
+          return curfile;
+        })
+      );
+      if (err instanceof Error) {
+        const error = JSON.parse(err.message);
+        if (Object.keys(error).includes('fileName')) {
+          const { message } = error;
+          setalertDetails({
+            showAlert: true,
+            alertType: 'error',
+            alertMessage: message,
+          });
+        }
+      }
+    }
+  };
+
+  const updatestatus = (i: statusupdate) => {
     const { file_name } = i;
     const {
       fileName,
@@ -416,7 +476,9 @@ const FileTable: React.FC<FileTableProps> = ({ isExpanded, connectionStatus, set
       );
     }
   }
-  function updateProgress(i: statusupdate) {
+
+
+  const updateProgress = (i: statusupdate) => {
     const { file_name } = i;
     const { fileName, nodeCount = 0, relationshipCount = 0, status, processed_chunk = 0, total_chunks } = file_name;
     if (fileName && total_chunks) {
@@ -437,7 +499,7 @@ const FileTable: React.FC<FileTableProps> = ({ isExpanded, connectionStatus, set
     }
   }
 
-  const pageSizeCalculation = Math.floor((currentOuterHeight - 402) / 45);
+  // const pageSizeCalculation = Math.floor((currentOuterHeight - 402) / 45);
 
   const table = useReactTable({
     data: filesData,
@@ -446,11 +508,11 @@ const FileTable: React.FC<FileTableProps> = ({ isExpanded, connectionStatus, set
     getFilteredRowModel: getFilteredRowModel(),
     getPaginationRowModel: getPaginationRowModel(),
     onColumnFiltersChange: setColumnFilters,
-    initialState: {
-      pagination: {
-        pageSize: pageSizeCalculation < 0 ? 9 : pageSizeCalculation,
-      },
-    },
+    // initialState: {
+    //   pagination: {
+    //     pageSize: pageSizeCalculation < 0 ? 9 : pageSizeCalculation,
+    //   },
+    // },
     state: {
       columnFilters,
       rowSelection,
@@ -458,7 +520,8 @@ const FileTable: React.FC<FileTableProps> = ({ isExpanded, connectionStatus, set
     onRowSelectionChange: setRowSelection,
     filterFns: {
       statusFilter: (row, columnId, filterValue) => {
-        return filterValue ? row.original[columnId] === 'New' : row.original[columnId];
+        const value = filterValue ? row.original[columnId] === 'New' : row.original[columnId];
+        return value;
       },
     },
     enableGlobalFilter: false,
@@ -471,20 +534,37 @@ const FileTable: React.FC<FileTableProps> = ({ isExpanded, connectionStatus, set
   });
 
   useEffect(() => {
-    const listener = (e: any) => {
-      setcurrentOuterHeight(e.currentTarget.outerHeight);
-      table.setPageSize(Math.floor((e.currentTarget.outerHeight - 402) / 45));
-    };
-    window.addEventListener('resize', listener);
-    return () => {
-      window.removeEventListener('resize', listener);
-    };
+    if (tableRef.current) {
+      // Component has content, calculate maximum height for table
+      // Observes the height of the content and calculates own height accordingly
+      const resizeObserver = new ResizeObserver((entries) => {
+        entries.forEach((entry) => {
+          const { height } = entry.contentRect;
+          // setcurrentOuterHeight(height);
+          const rowHeight = document?.getElementsByClassName('ndl-data-grid-td')?.[0]?.clientHeight ?? 69;
+          table.setPageSize(Math.floor((height) / rowHeight));
+        });
+      });
+
+      const contentElement = document.getElementsByClassName('ndl-data-grid-scrollable')[0];
+      resizeObserver.observe(contentElement);
+
+      return () => {
+        // Stop observing content after cleanup
+        resizeObserver.unobserve(contentElement);
+      };
+    }
   }, []);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     table.getColumn('status')?.setFilterValue(e.target.checked);
+    if (!table.getCanNextPage() || table.getRowCount()) {
+      table.setPageIndex(0);
+    }
   };
+
   const classNameCheck = isExpanded ? 'fileTableWithExpansion' : `filetable`;
+
   const handleClose = () => {
     setalertDetails((prev) => ({ ...prev, showAlert: false }));
     localStorage.setItem('alertShown', JSON.stringify(true));
@@ -492,6 +572,7 @@ const FileTable: React.FC<FileTableProps> = ({ isExpanded, connectionStatus, set
   useEffect(() => {
     setSelectedRows(table.getSelectedRowModel().rows.map((i) => i.id));
   }, [table.getSelectedRowModel()]);
+
   return (
     <>
       {alertDetails.showAlert && (
@@ -510,6 +591,7 @@ const FileTable: React.FC<FileTableProps> = ({ isExpanded, connectionStatus, set
           </div>
           <div className={`${isExpanded ? 'w-[calc(100%-64px)]' : 'mx-auto w-[calc(100%-100px)]'}`}>
             <DataGrid
+              ref={tableRef}
               isResizable={true}
               tableInstance={table}
               styling={{
