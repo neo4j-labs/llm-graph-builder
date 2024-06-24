3a7ab4541b02341b2c2aa9ea1e0462a2f31dcecf
vasanthasaikalluri <165021735+vasanthasaikalluri@users.noreply.github.com>
2024-06-24 14:28:43 +0000

    Moved the templates to constants

--- backend/src/QA_integration_new.py
+++ backend/src/QA_integration_new.py
@@ -2,9 +2,6 @@ from langchain_community.vectorstores.neo4j_vector import Neo4jVector
 from langchain.graphs import Neo4jGraph
 import os
 from dotenv import load_dotenv
-from langchain_openai import ChatOpenAI
-from langchain_google_vertexai import ChatVertexAI
-from langchain_google_vertexai import HarmBlockThreshold, HarmCategory
 import logging
 from langchain_community.chat_message_histories import Neo4jChatMessageHistory
 from src.shared.common_fn import load_embedding_model, get_llm
@@ -29,67 +26,12 @@ EMBEDDING_MODEL = os.getenv('EMBEDDING_MODEL')
 EMBEDDING_FUNCTION , _ = load_embedding_model(EMBEDDING_MODEL)
 
 
-RETRIEVAL_QUERY = """
-WITH node as chunk, score
-MATCH (chunk)-[:PART_OF]->(d:Document)
-WITH d, collect(distinct {chunk: chunk, score: score}) as chunks, avg(score) as score
-WITH d, score, 
-     [c in chunks | c.chunk.text] as texts, 
-     [c in chunks | {id: c.chunk.id, score: c.score}] as chunkdetails
-WITH d, score, texts, chunkdetails,
-     apoc.text.join(texts, "\n----\n") as text
-RETURN text, score, 
-       {source: COALESCE(CASE WHEN d.url CONTAINS "None" THEN d.fileName ELSE d.url END, d.fileName), chunkdetails: chunkdetails} as metadata
-"""
-
-SYSTEM_TEMPLATE = """
-You are an AI-powered question-answering agent. Your task is to provide accurate and comprehensive responses to user queries based on the given context, chat history, and available resources.
-
-### Response Guidelines:
-1. **Direct Answers**: Provide clear and thorough answers to the user's queries without headers unless requested. Avoid speculative responses.
-2. **Utilize History and Context**: Leverage relevant information from previous interactions, the current user input, and the context provided below.
-3. **No Greetings in Follow-ups**: Start with a greeting in initial interactions. Avoid greetings in subsequent responses unless there's a significant break or the chat restarts.
-4. **Admit Unknowns**: Clearly state if an answer is unknown. Avoid making unsupported statements.
-5. **Avoid Hallucination**: Only provide information based on the context provided. Do not invent information.
-6. **Response Length**: Keep responses concise and relevant. Aim for clarity and completeness within 4-5 sentences unless more detail is requested.
-7. **Tone and Style**: Maintain a professional and informative tone. Be friendly and approachable.
-8. **Error Handling**: If a query is ambiguous or unclear, ask for clarification rather than providing a potentially incorrect answer.
-9. **Fallback Options**: If the required information is not available in the provided context, provide a polite and helpful response. Example: "I don't have that information right now." or "I'm sorry, but I don't have that information. Is there something else I can help with?"
-10. **Context Availability**: If the context is empty, do not provide answers based solely on internal knowledge. Instead, respond appropriately by indicating the lack of information.
-
-
-**IMPORTANT** : DO NOT ANSWER FROM YOUR KNOWLEDGE BASE USE THE BELOW CONTEXT
-
-### Context:
-<context>
-{context}
-</context>
-
-### Example Responses:
-User: Hi 
-AI Response: 'Hello there! How can I assist you today?'
-
-User: "What is Langchain?"
-AI Response: "Langchain is a framework that enables the development of applications powered by large language models, such as chatbots. It simplifies the integration of language models into various applications by providing useful tools and components."
-
-User: "Can you explain how to use memory management in Langchain?"
-AI Response: "Langchain's memory management involves utilizing built-in mechanisms to manage conversational context effectively. It ensures that the conversation remains coherent and relevant by maintaining the history of interactions and using it to inform responses."
-
-User: "I need help with PyCaret's classification model."
-AI Response: "PyCaret simplifies the process of building and deploying machine learning models. For classification tasks, you can use PyCaret's setup function to prepare your data. After setup, you can compare multiple models to find the best one, and then fine-tune it for better performance."
-
-User: "What can you tell me about the latest realtime trends in AI?"
-AI Response: "I don't have that information right now. Is there something else I can help with?"
-
-Note: This system does not generate answers based solely on internal knowledge. It answers from the information provided in the user's current and previous inputs, and from the context.
-"""
-
 def get_neo4j_retriever(graph, index_name="vector", search_k=CHAT_SEARCH_KWARG_K, score_threshold=CHAT_SEARCH_KWARG_SCORE_THRESHOLD):
     try:
         neo_db = Neo4jVector.from_existing_index(
             embedding=EMBEDDING_FUNCTION,
             index_name=index_name,
-            retrieval_query=RETRIEVAL_QUERY,
+            retrieval_query=VECTOR_SEARCH_QUERY,
             graph=graph
         )
         logging.info(f"Successfully retrieved Neo4jVector index '{index_name}'")
@@ -101,11 +43,9 @@ def get_neo4j_retriever(graph, index_name="vector", search_k=CHAT_SEARCH_KWARG_K
         return None 
     
 def create_document_retriever_chain(llm,retriever):
-    question_template= "Given the below conversation, generate a search query to look up in order to get information relevant to the conversation. Only respond with the query, nothing else."
-
     query_transform_prompt = ChatPromptTemplate.from_messages(
         [
-            ("system", question_template),
+            ("system", QUESTION_TRANSFORM_TEMPLATE),
             MessagesPlaceholder(variable_name="messages")
         ]
     )
@@ -172,7 +112,7 @@ def format_documents(documents):
 
     return "\n\n".join(formatted_docs), sources
 
-def get_rag_chain(llm,system_template=SYSTEM_TEMPLATE):
+def get_rag_chain(llm,system_template=CHAT_SYSTEM_TEMPLATE):
     question_answering_prompt = ChatPromptTemplate.from_messages(
     [
         ("system", system_template),
@@ -207,7 +147,6 @@ def get_sources_and_chunks(sources_used, docs):
 def summarize_messages(llm,history,stored_messages):
     if len(stored_messages) == 0:
         return False
-    # summarization_template = "Distill the below chat messages into a single summary message. Include as many specific details as you can."
     summarization_prompt = ChatPromptTemplate.from_messages(
         [
             MessagesPlaceholder(variable_name="chat_history"),
