69ee9605eb6ebaba15ad5e62c4522c0234e500b7
Prakriti Solankey <156313631+prakriti-solankey@users.noreply.github.com>
2024-07-05 09:34:48 +0000

    Revert "chatmode changes"
    This reverts commit 60e32c34e8f8c61c11ec929a30f25c388b34624d.

--- frontend/src/components/ChatBot/Chatbot.tsx
+++ frontend/src/components/ChatBot/Chatbot.tsx
@@ -340,7 +340,6 @@ const Chatbot: React.FC<ChatbotProps> = (props) => {
           chunk_ids={chunkModal}
           response_time={responseTime}
           total_tokens={tokensUsed}
-          mode={chatMode}
         />
       </Modal>
     </div>
