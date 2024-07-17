4fb3a2e3e082080558874ef9632368b9d7fd3a00
kartikpersistent <101251502+kartikpersistent@users.noreply.github.com>
2024-07-17 15:30:19 +0000

    removed unsued states

--- frontend/src/components/QuickStarter.tsx
+++ frontend/src/components/QuickStarter.tsx
@@ -11,7 +11,6 @@ const QuickStarter: React.FunctionComponent = () => {
   const themeUtils = React.useContext(ThemeWrapperContext);
   const [themeMode, setThemeMode] = useState<string>(themeUtils.colorMode);
   const [showSettingsModal, setshowSettingsModal] = useState<boolean>(false);
-  const [showOrphanNodeDeletionDialog, setshowOrphanNodeDeletionDialog] = useState<boolean>(false);
 
   const toggleColorMode = () => {
     setThemeMode((prevThemeMode) => {
@@ -25,12 +24,7 @@ const QuickStarter: React.FunctionComponent = () => {
   const closeSettingModal = () => {
     setshowSettingsModal(false);
   };
-  const openOrphanNodeDeletionModal = () => {
-    setshowOrphanNodeDeletionDialog(true);
-  };
-  const closeOrphanNodeDeletionModal = () => {
-    setshowOrphanNodeDeletionDialog(false);
-  };
+
   return (
     <UserCredentialsWrapper>
       <FileContextProvider>
