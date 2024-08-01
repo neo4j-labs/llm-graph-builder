c5526d64051c986591fd9ba8926b5f2974d95511
aashipandya <156318202+aashipandya@users.noreply.github.com>
2024-08-01 18:04:07 +0530

    DO NOT MERGE - Document, chunk node labels and relation labels updated with underscore (#626)
    * Document and Chunk node label updated
    
    * added underscores for all relationship types
    
    * updated queries from DEV
    
    * updated Document and Chunk nodes after DEV merge
    
    * Label Changes
    
    * Exclude labels from schema API which label start from __
    
    ---------
    
    Co-authored-by: Prakriti Solankey <156313631+prakriti-solankey@users.noreply.github.com>
    Co-authored-by: Pravesh Kumar <121786590+praveshkumar1988@users.noreply.github.com>

--- frontend/src/utils/Constants.ts
+++ frontend/src/utils/Constants.ts
@@ -3,31 +3,31 @@ import { GraphType, OptionType } from '../types';
 
 export const document = `+ [docs]`;
 
-export const chunks = `+ collect { MATCH p=(c)-[:NEXT_CHUNK]-() RETURN p } // chunk-chain
-+ collect { MATCH p=(c)-[:SIMILAR]-() RETURN p } // similar-chunks`;
+export const chunks = `+ collect { MATCH p=(c)-[:__NEXT_CHUNK__]-() RETURN p } // chunk-chain
++ collect { MATCH p=(c)-[:__SIMILAR__]-() RETURN p } // similar-chunks`;
 
-export const entities = `+ collect { OPTIONAL MATCH (c:Chunk)-[:HAS_ENTITY]->(e), p=(e)-[*0..1]-(:!Chunk) RETURN p}`;
+export const entities = `+ collect { OPTIONAL MATCH (c:__Chunk__)-[:__HAS_ENTITY__]->(e), p=(e)-[*0..1]-(:!__Chunk__) RETURN p}`;
 
 export const docEntities = `+ [docs] 
-+ collect { MATCH (c:Chunk)-[:HAS_ENTITY]->(e), p=(e)--(:!Chunk) RETURN p }`;
++ collect { MATCH (c:__Chunk__)-[:__HAS_ENTITY__]->(e), p=(e)--(:!__Chunk__) RETURN p }`;
 
 export const docChunks = `+[chunks]
-+collect {MATCH p=(c)-[:FIRST_CHUNK]-() RETURN p} //first chunk
-+ collect { MATCH p=(c)-[:NEXT_CHUNK]-() RETURN p } // chunk-chain
-+ collect { MATCH p=(c)-[:SIMILAR]-() RETURN p } // similar-chunk`;
++collect {MATCH p=(c)-[:__FIRST_CHUNK__]-() RETURN p} //first chunk
++ collect { MATCH p=(c)-[:__NEXT_CHUNK__]-() RETURN p } // chunk-chain
++ collect { MATCH p=(c)-[:__SIMILAR__]-() RETURN p } // similar-chunk`;
 
-export const chunksEntities = `+ collect { MATCH p=(c)-[:NEXT_CHUNK]-() RETURN p } // chunk-chain
+export const chunksEntities = `+ collect { MATCH p=(c)-[:__NEXT_CHUNK__]-() RETURN p } // chunk-chain
 
-+ collect { MATCH p=(c)-[:SIMILAR]-() RETURN p } // similar-chunks
++ collect { MATCH p=(c)-[:__SIMILAR__]-() RETURN p } // similar-chunks
 //chunks with entities
-+ collect { OPTIONAL MATCH p=(c:Chunk)-[:HAS_ENTITY]->(e)-[*0..1]-(:!Chunk) RETURN p }`;
++ collect { OPTIONAL MATCH p=(c:__Chunk__)-[:__HAS_ENTITY__]->(e)-[*0..1]-(:!__Chunk__) RETURN p }`;
 
 export const docChunkEntities = `+[chunks]
-+collect {MATCH p=(c)-[:FIRST_CHUNK]-() RETURN p} //first chunk
-+ collect { MATCH p=(c)-[:NEXT_CHUNK]-() RETURN p } // chunk-chain
-+ collect { MATCH p=(c)-[:SIMILAR]-() RETURN p } // similar-chunks
++collect {MATCH p=(c)-[:__FIRST_CHUNK__]-() RETURN p} //first chunk
++ collect { MATCH p=(c)-[:__NEXT_CHUNK__]-() RETURN p } // chunk-chain
++ collect { MATCH p=(c)-[:__SIMILAR__]-() RETURN p } // similar-chunks
 //chunks with entities
-+ collect { OPTIONAL MATCH p=(c:Chunk)-[:HAS_ENTITY]->(e)-[*0..1]-(:!Chunk) RETURN p }`;
++ collect { OPTIONAL MATCH p=(c:__Chunk__)-[:__HAS_ENTITY__]->(e)-[*0..1]-(:!__Chunk__) RETURN p }`;
 export const APP_SOURCES =
   process.env.REACT_APP_SOURCES !== ''
     ? (process.env.REACT_APP_SOURCES?.split(',') as string[])
