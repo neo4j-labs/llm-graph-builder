7c7ce5459889014d0daa84ce23f6d8d97a61d571
Prakriti Solankey <156313631+prakriti-solankey@users.noreply.github.com>
2024-08-12 06:27:33 +0000

    build changes where workspaces/llm-graph-builder/frontend/node_modules/@segment/analytics-next/dist/pkg/lib/get-process-env.js file was breaking

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,15 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // see https://stackoverflow.com/questions/73834404/react-uncaught-referenceerror-process-is-not-defined
 // otherwise use import.meta.env.VITE_BACKEND_API_URL and expose it as such with the VITE_ prefix
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
+  // const env = loadEnv(mode, process.cwd(), '');
   return {
-    define: {
-      'process.env': env,
+    optimizeDeps: {
+      esbuildOptions: {
+        target: 'es2020'
+      }
     },
     plugins: [react()],
   };
