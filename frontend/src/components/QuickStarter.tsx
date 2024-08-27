0cd67e5404c0b3b5da4d6d590f5b23ea0ba73116
kartikpersistent <101251502+kartikpersistent@users.noreply.github.com>
2024-08-27 13:29:51 +0000

    merge fixes

--- frontend/src/components/QuickStarter.tsx
+++ frontend/src/components/QuickStarter.tsx
@@ -10,7 +10,6 @@ const QuickStarter: React.FunctionComponent = () => {
   const themeUtils = React.useContext(ThemeWrapperContext);
   const [themeMode, setThemeMode] = useState<string>(themeUtils.colorMode);
   const [showSettingsModal, setshowSettingsModal] = useState<boolean>(false);
-  const [showOrphanNodeDeletionDialog, setshowOrphanNodeDeletionDialog] = useState<boolean>(false);
 
   const toggleColorMode = () => {
     setThemeMode((prevThemeMode) => {
@@ -24,12 +23,6 @@ const QuickStarter: React.FunctionComponent = () => {
   const closeSettingModal = () => {
     setshowSettingsModal(false);
   };
-  const openOrphanNodeDeletionModal = () => {
-    setshowOrphanNodeDeletionDialog(true);
-  };
-  const closeOrphanNodeDeletionModal = () => {
-    setshowOrphanNodeDeletionDialog(false);
-  };
   return (
     <UserCredentialsWrapper>
       <FileContextProvider>
@@ -40,9 +33,6 @@ const QuickStarter: React.FunctionComponent = () => {
               openSettingsDialog={openSettingsModal}
               isSettingPanelExpanded={showSettingsModal}
               closeSettingModal={closeSettingModal}
-              closeOrphanNodeDeletionModal={closeOrphanNodeDeletionModal}
-              showOrphanNodeDeletionModal={showOrphanNodeDeletionDialog}
-              openOrphanNodeDeletionModal={openOrphanNodeDeletionModal}
             />
           </AlertContextWrapper>
         </MessageContextWrapper>
