f3a4185f6be53c5b07de9373f3e13e1206605091
Prakriti Solankey <156313631+prakriti-solankey@users.noreply.github.com>
2024-08-30 05:37:33 +0000

    review comment resolved

--- frontend/src/context/UserCredentials.tsx
+++ frontend/src/context/UserCredentials.tsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useContext, FunctionComponent, ReactNode } from 'react';
+import { createContext, useState, useContext, FunctionComponent, ReactNode, useReducer } from 'react';
 import { ContextProps, UserCredentials } from '../types';
 
 type Props = {
@@ -17,7 +17,7 @@ export const useCredentials = () => {
 };
 const UserCredentialsWrapper: FunctionComponent<Props> = (props) => {
   const [userCredentials, setUserCredentials] = useState<UserCredentials | null>(null);
-  const [connectionStatus, setConnectionStatus] = useState<boolean>(false);
+  const [connectionStatus, setConnectionStatus] = useReducer((state) => !state, false);
   const value = {
     userCredentials,
     setUserCredentials,
