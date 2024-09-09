ccff6a4111e35d85f83fc47d83d32071a692ba29
abhishekkumar-27 <164544129+abhishekkumar-27@users.noreply.github.com>
2024-09-09 14:25:08 +0000

    added chnages to graph schema

--- backend/test_integrationqa.py
+++ backend/test_integrationqa.py
@@ -64,9 +64,9 @@ def test_graph_from_file_local(model_name):
 
 def test_graph_from_wikipedia(model_name):
     """Test graph creation from a Wikipedia page."""
-    wiki_query = 'https://en.wikipedia.org/wiki/Ram_Mandir'
+    wiki_query = 'https://en.wikipedia.org/wiki/Google_DeepMind'
     source_type = 'Wikipedia'
-    file_name = "Ram_Mandir"
+    file_name = "Google_DeepMind"
     create_source_node_graph_url_wikipedia(graph, model_name, wiki_query, source_type)
 
     wiki_result = extract_graph_from_file_Wikipedia(
@@ -140,20 +140,18 @@ def compare_graph_results(results):
 def run_tests():
     final_list = []
     error_list = []
-    models = [
-        'openai-gpt-3.5', 'openai-gpt-4o', 'openai-gpt-4o-mini', 'azure_ai_gpt_35',
-        'azure_ai_gpt_4o', 'anthropic_claude_3_5_sonnet', 'fireworks_v3p1_405b',
-        'fireworks_llama_v3_70b', 'ollama_llama3', 'bedrock_claude_3_5_sonnet'
-    ]
+    models = ['openai-gpt-4o','gemini-1.5-pro']
 
     for model_name in models:
         try:
-            final_list.append(test_graph_from_file_local(model_name))
-            final_list.append(test_graph_from_wikipedia(model_name))
-            final_list.append(test_graph_from_youtube_video(model_name))
-            final_list.append(test_chatbot_qna(model_name))
-            final_list.append(test_chatbot_qna(model_name, mode='vector'))
-            final_list.append(test_chatbot_qna(model_name, mode='hybrid'))
+              final_list.append(test_graph_from_file_local(model_name))
+              final_list.append(test_graph_from_wikipedia(model_name))
+              final_list.append(test_populate_graph_schema_from_text(model_name))
+              final_list.append(test_graph_website(model_name))
+            #   final_list.append(test_graph_from_youtube_video(model_name))
+            #   final_list.append(test_chatbot_qna(model_name))
+            #   final_list.append(test_chatbot_qna(model_name, mode='vector'))
+            #   final_list.append(test_chatbot_qna(model_name, mode='graph+vector+fulltext'))
         except Exception as e:
             error_list.append((model_name, str(e)))
     #Compare and log diffrences in graph results
