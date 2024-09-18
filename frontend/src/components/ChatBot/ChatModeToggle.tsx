3c7e6200ab5070dd79221b672aad5f72650c648d
Prakriti Solankey <156313631+prakriti-solankey@users.noreply.github.com>
2024-09-18 13:10:37 +0000

    mode disable changes

--- frontend/src/components/ChatBot/ChatModeToggle.tsx
+++ frontend/src/components/ChatBot/ChatModeToggle.tsx
@@ -1,5 +1,5 @@
 import { StatusIndicator, Typography } from '@neo4j-ndl/react';
-import { useEffect, useMemo } from 'react';
+import { useMemo } from 'react';
 import { useFileContext } from '../../context/UsersFiles';
 import CustomMenu from '../UI/Menu';
 import { chatModes } from '../../utils/Constants';
@@ -8,7 +8,7 @@ import { capitalizeWithPlus } from '../../utils/Utils';
 import { useCredentials } from '../../context/UserCredentials';
 export default function ChatModeToggle({
   menuAnchor,
-  closeHandler = () => {},
+  closeHandler = () => { },
   open,
   anchorPortal = true,
   disableBackdrop = false,
@@ -28,39 +28,42 @@ export default function ChatModeToggle({
       : chatModes?.filter((m) => !m.mode.includes('entity search+vector'));
   }, [isGdsActive, isCommunityAllowed]);
 
-  useEffect(() => {
-    if (selectedRows.length && chatMode !== 'graph+vector') {
-      setchatMode('graph+vector');
-    }
-  }, [selectedRows, chatMode, setchatMode]);
 
   const menuItems = useMemo(() => {
-    return memoizedChatModes?.map((m) => ({
-      title: (
-        <div>
-          <Typography variant='subheading-small'>
-            {m.mode.includes('+') ? capitalizeWithPlus(m.mode) : capitalize(m.mode)}
-          </Typography>
+    return memoizedChatModes?.map((m) => {
+      const isDisabled = Boolean(selectedRows.length && !(m.mode === 'vector' || m.mode === 'graph+vector'));
+      return {
+        title: (
           <div>
-            <Typography variant='body-small'>{m.description}</Typography>
+            <Typography variant='subheading-small'>
+              {m.mode.includes('+') ? capitalizeWithPlus(m.mode) : capitalize(m.mode)}
+            </Typography>
+            <div>
+              <Typography variant='body-small'>{m.description}</Typography>
+            </div>
           </div>
-        </div>
-      ),
-      onClick: () => {
-        setchatMode(m.mode);
-        closeHandler();
-      },
-      disabledCondition: Boolean(selectedRows.length && !(m.mode === 'vector' || m.mode === 'graph+vector')),
-      description: (
-        <span>
-          {chatMode === m.mode && (
-            <>
-              <StatusIndicator type='success' /> Selected
-            </>
-          )}
-        </span>
-      ),
-    }));
+        ),
+        onClick: () => {
+          setchatMode(m.mode);
+          closeHandler();
+        },
+        disabledCondition: isDisabled,
+        description: (
+          <span>
+            {chatMode === m.mode && (
+              <>
+                <StatusIndicator type='success' /> Selected
+              </>
+            )}
+            {isDisabled && (
+              <>
+                <StatusIndicator type='warning' /> Chatmode not available
+              </>
+            )}
+          </span>
+        ),
+      };
+    });
   }, [chatMode, memoizedChatModes, setchatMode, closeHandler, selectedRows]);
   return (
     <CustomMenu
