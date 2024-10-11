f5054888d3db335769ba3af5a9bf185526da5026
kartikpersistent <101251502+kartikpersistent@users.noreply.github.com>
2024-10-11 06:32:01 +0000

    build fix

--- frontend/src/components/Content.tsx
+++ frontend/src/components/Content.tsx
@@ -105,7 +105,7 @@ const Content: React.FC<ContentProps> = ({
     queue,
     processedCount,
     setProcessedCount,
-    setchatMode,
+    setchatModes
   } = useFileContext();
   const [viewPoint, setViewPoint] = useState<'tableView' | 'showGraphView' | 'chatInfoView'>('tableView');
   const [showDeletePopUp, setshowDeletePopUp] = useState<boolean>(false);
@@ -552,7 +552,7 @@ const Content: React.FC<ContentProps> = ({
     setSelectedNodes([]);
     setSelectedRels([]);
     setClearHistoryData(true);
-    setchatMode(chatModeLables.graph_vector_fulltext);
+    setchatModes([chatModeLables.graph_vector_fulltext]);
   };
 
   const retryHandler = async (filename: string, retryoption: string) => {
