331ed01e47b0ef389b1e1b52f6a5cfc3b8634451
kartikpersistent <101251502+kartikpersistent@users.noreply.github.com>
2024-11-12 11:24:45 +0000

    added middleware

--- backend/score.py
+++ backend/score.py
@@ -19,7 +19,7 @@ from sse_starlette.sse import EventSourceResponse
 from src.communities import create_communities
 from src.neighbours import get_neighbour_nodes
 import json
-from typing import List, Mapping
+from typing import List, Mapping, Union
 from starlette.middleware.sessions import SessionMiddleware
 import google_auth_oauthlib.flow
 from google.oauth2.credentials import Credentials
@@ -33,8 +33,10 @@ from Secweb.StrictTransportSecurity import HSTS
 from Secweb.ContentSecurityPolicy import ContentSecurityPolicy
 from Secweb.XContentTypeOptions import XContentTypeOptions
 from Secweb.XFrameOptions import XFrame
-#from fastapi.middleware.gzip import GZipMiddleware
+from fastapi.middleware.gzip import GZipMiddleware
 from src.ragas_eval import *
+from starlette.types import ASGIApp, Message, Receive, Scope, Send
+import gzip
 
 logger = CustomLogger()
 CHUNK_DIR = os.path.join(os.path.dirname(__file__), "chunks")
@@ -49,14 +51,42 @@ def healthy():
 
 def sick():
     return False
-
+class CustomGZipMiddleware:
+    def __init__(
+        self,
+        app: ASGIApp,
+        paths: List[str],
+        minimum_size: int = 1000,
+        compresslevel: int = 5
+    ):
+        self.app = app
+        self.paths = paths
+        self.minimum_size = minimum_size
+        self.compresslevel = compresslevel
+    
+    async def __call__(self, scope: Scope, receive: Receive, send: Send):
+        if scope["type"] != "http":
+            return await self.app(scope, receive, send)
+ 
+        path = scope["path"]
+        should_compress = any(path.startswith(gzip_path) for gzip_path in self.paths)
+        
+        if not should_compress:
+            return await self.app(scope, receive, send)
+        
+        gzip_middleware = GZipMiddleware(
+            app=self.app,
+            minimum_size=self.minimum_size,
+            compresslevel=self.compresslevel
+        )
+        await gzip_middleware(scope, receive, send)
 app = FastAPI()
 # SecWeb(app=app, Option={'referrer': False, 'xframe': False})
 app.add_middleware(ContentSecurityPolicy, Option={'default-src': ["'self'"], 'base-uri': ["'self'"], 'block-all-mixed-content': []}, script_nonce=False, style_nonce=False, report_only=False)
 app.add_middleware(XContentTypeOptions)
 app.add_middleware(XFrame, Option={'X-Frame-Options': 'DENY'})
 #app.add_middleware(GZipMiddleware, minimum_size=1000, compresslevel=5)
-
+app.add_middleware(CustomGZipMiddleware, minimum_size=1000, compresslevel=5,paths=["/sources_list","/url/scan","/extract","/chat_bot","/chunk_entities","/get_neighbours","/graph_query","/schema","/populate_graph_schema","/get_unconnected_nodes_list","/get_duplicate_nodes","/fetch_chunktext"])
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
