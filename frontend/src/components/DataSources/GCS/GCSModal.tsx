56a7526d7f5cb9b38e40fc83b5db5ee91197f15a
kartikpersistent <101251502+kartikpersistent@users.noreply.github.com>
2024-11-15 12:05:15 +0000

    Integrated the node count and relationship breakdowns

--- frontend/src/components/DataSources/GCS/GCSModal.tsx
+++ frontend/src/components/DataSources/GCS/GCSModal.tsx
@@ -33,6 +33,12 @@ const GCSModal: React.FC<GCSModalProps> = ({ hideModal, open, openGCSModal }) =>
     processingProgress: undefined,
     retryOption: '',
     retryOptionStatus: false,
+    chunkNodeCount: 0,
+    chunkRelCount: 0,
+    entityNodeCount: 0,
+    entityEntityRelCount: 0,
+    communityNodeCount: 0,
+    communityRelCount: 0,
   };
 
   const reset = () => {
