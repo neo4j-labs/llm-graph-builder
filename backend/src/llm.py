1f937790b2bf3b8675a9cd7a62f2ee45f3d130ad
aashipandya <156318202+aashipandya@users.noreply.github.com>
2024-11-27 09:10:40 +0000

    combining one chunk

--- backend/src/llm.py
+++ backend/src/llm.py
@@ -17,7 +17,15 @@ from langchain_community.chat_models import ChatOllama
 import boto3
 import google.auth
 from src.shared.constants import MODEL_VERSIONS, PROMPT_TO_ALL_LLMs
-
+from typing import Any, Dict, List, Optional, Sequence, Tuple, Type, Union, cast
+from langchain_core.messages import SystemMessage
+from langchain_core.output_parsers import JsonOutputParser
+from langchain_core.prompts import (
+    ChatPromptTemplate,
+    HumanMessagePromptTemplate,
+    PromptTemplate,
+)
+from pydantic import BaseModel, Field, create_model
 
 def get_llm(model: str):
     """Retrieve the specified language model based on the model name."""
@@ -167,7 +175,7 @@ async def get_graph_document_list(
             node_properties = False
             relationship_properties = False
         else:
-            node_properties = ["description"]
+            node_properties = ["description","date","year","time"]
             relationship_properties = ["description"]
         llm_transformer = LLMGraphTransformer(
             llm=llm,
@@ -177,6 +185,7 @@ async def get_graph_document_list(
             allowed_relationships=allowedRelationship,
             ignore_tool_usage=True,
             #prompt = ChatPromptTemplate.from_messages(["system",PROMPT_TO_ALL_LLMs])
+            prompt = create_unstructured_prompt(allowedNodes, allowedRelationship)
         )
     # with ThreadPoolExecutor(max_workers=10) as executor:
     #     for chunk in combined_chunk_document_list:
@@ -201,8 +210,8 @@ async def get_graph_document_list(
 async def get_graph_from_llm(model, chunkId_chunkDoc_list, allowedNodes, allowedRelationship):
     
     llm, model_name = get_llm(model)
-    #combined_chunk_document_list = get_combined_chunks(chunkId_chunkDoc_list)
-    combined_chunk_document_list = get_chunk_id_as_doc_metadata(chunkId_chunkDoc_list)
+    combined_chunk_document_list = get_combined_chunks(chunkId_chunkDoc_list)
+    #combined_chunk_document_list = get_chunk_id_as_doc_metadata(chunkId_chunkDoc_list)
     
     if  allowedNodes is None or allowedNodes=="":
         allowedNodes =[]
@@ -217,3 +226,183 @@ async def get_graph_from_llm(model, chunkId_chunkDoc_list, allowedNodes, allowed
         llm, combined_chunk_document_list, allowedNodes, allowedRelationship
     )
     return graph_document_list
+
+
+examples = [
+    {
+        "text": (
+            "Adam is a software engineer in Microsoft since 2009, "
+            "and last year he got an award as the Best Talent"
+        ),
+        "head": "Adam",
+        "head_type": "Person",
+        "relation": "WORKS_FOR",
+        "tail": "Microsoft",
+        "tail_type": "Company",
+    },
+    {
+        "text": (
+            "Adam is a software engineer in Microsoft since 2009, "
+            "and last year he got an award as the Best Talent"
+        ),
+        "head": "Adam",
+        "head_type": "Person",
+        "relation": "HAS_AWARD",
+        "tail": "Best Talent",
+        "tail_type": "Award",
+    },
+    {
+        "text": (
+            "Microsoft is a tech company that provide "
+            "several products such as Microsoft Word"
+        ),
+        "head": "Microsoft Word",
+        "head_type": "Product",
+        "relation": "PRODUCED_BY",
+        "tail": "Microsoft",
+        "tail_type": "Company",
+    },
+    {
+        "text": "Microsoft Word is a lightweight app that accessible offline",
+        "head": "Microsoft Word",
+        "head_type": "Product",
+        "relation": "HAS_CHARACTERISTIC",
+        "tail": "lightweight app",
+        "tail_type": "Characteristic",
+    },
+    {
+        "text": "Microsoft Word is a lightweight app that accessible offline",
+        "head": "Microsoft Word",
+        "head_type": "Product",
+        "relation": "HAS_CHARACTERISTIC",
+        "tail": "accessible offline",
+        "tail_type": "Characteristic",
+    },
+]
+
+def create_unstructured_prompt(
+    node_labels: Optional[List[str]] = None,
+    rel_types: Optional[Union[List[str], List[Tuple[str, str, str]]]] = None,
+    relationship_type: Optional[str] = None,
+) -> ChatPromptTemplate:
+    node_labels_str = str(node_labels) if node_labels else ""
+    if rel_types:
+        if relationship_type == "tuple":
+            rel_types_str = str(list({item[1] for item in rel_types}))
+        else:
+            rel_types_str = str(rel_types)
+    else:
+        rel_types_str = ""
+    base_string_parts = [
+        "You are a top-tier algorithm designed for extracting information in "
+        "structured formats to build a knowledge graph. Your task is to identify "
+        "the entities and relations requested with the user prompt from a given "
+        "text. You must generate the output in a JSON format containing a list "
+        'with JSON objects. Each object should have the keys: "head", '
+        '"head_type", "relation", "tail", and "tail_type". The "head" '
+        "key must contain the text of the extracted entity with one of the types "
+        "from the provided list in the user prompt.",
+        f'The "head_type" key must contain the type of the extracted head entity, '
+        f"which must be one of the types from {node_labels_str}."
+        if node_labels
+        else "",
+        f'The "relation" key must contain the type of relation between the "head" '
+        f'and the "tail", which must be one of the relations from {rel_types_str}.'
+        if rel_types
+        else "",
+        f'The "tail" key must represent the text of an extracted entity which is '
+        f'the tail of the relation, and the "tail_type" key must contain the type '
+        f"of the tail entity from {node_labels_str}."
+        if node_labels
+        else "",
+        "Your task is to extract relationships from text strictly adhering "
+        "to the provided schema. The relationships can only appear "
+        "between specific node types are presented in the schema format "
+        "like: (Entity1Type, RELATIONSHIP_TYPE, Entity2Type) /n"
+        f"Provided schema is {rel_types}"
+        if relationship_type == "tuple"
+        else "",
+        "Attempt to extract as many entities and relations as you can. Maintain "
+        "Entity Consistency: When extracting entities, it's vital to ensure "
+        'consistency. If an entity, such as "John Doe", is mentioned multiple '
+        "times in the text but is referred to by different names or pronouns "
+        '(e.g., "Joe", "he"), always use the most complete identifier for '
+        "that entity. The knowledge graph should be coherent and easily "
+        "understandable, so maintaining consistency in entity references is "
+        "crucial.",
+        "IMPORTANT NOTES:\n- Don't add any explanation and text.",
+    ]
+    system_prompt = "\n".join(filter(None, base_string_parts))
+
+    system_message = SystemMessage(content=system_prompt)
+    parser = JsonOutputParser(pydantic_object=UnstructuredRelation)
+
+    human_string_parts = [
+        "Based on the following example, extract entities and "
+        "relations from the provided text.\n\n",
+        "Use the following entity types, don't use other entity "
+        "that is not defined below:"
+        "# ENTITY TYPES:"
+        "{node_labels}"
+        if node_labels
+        else "",
+        "Use the following relation types, don't use other relation "
+        "that is not defined below:"
+        "# RELATION TYPES:"
+        "{rel_types}"
+        if rel_types
+        else "",
+        "Your task is to extract relationships from text strictly adhering "
+        "to the provided schema. The relationships can only appear "
+        "between specific node types are presented in the schema format "
+        "like: (Entity1Type, RELATIONSHIP_TYPE, Entity2Type) /n"
+        f"Provided schema is {rel_types}"
+        if relationship_type == "tuple"
+        else "",
+        "Below are a number of examples of text and their extracted "
+        "entities and relationships."
+        "{examples}\n"
+        "For the following text, extract entities and relations as "
+        "in the provided example."
+        "{format_instructions}\nText: {input}",
+    ]
+    human_prompt_string = "\n".join(filter(None, human_string_parts))
+    human_prompt = PromptTemplate(
+        template=human_prompt_string,
+        input_variables=["input"],
+        partial_variables={
+            "format_instructions": parser.get_format_instructions(),
+            "node_labels": node_labels,
+            "rel_types": rel_types,
+            "examples": examples,
+        },
+    )
+
+    human_message_prompt = HumanMessagePromptTemplate(prompt=human_prompt)
+
+    chat_prompt = ChatPromptTemplate.from_messages(
+        [system_message, human_message_prompt]
+    )
+    return chat_prompt
+
+
+class UnstructuredRelation(BaseModel):
+    head: str = Field(
+        description=(
+            "extracted head entity like Microsoft, Apple, John. "
+            "Must use human-readable unique identifier."
+        )
+    )
+    head_type: str = Field(
+        description="type of the extracted head entity like Person, Company, etc"
+    )
+    relation: str = Field(description="relation between the head and the tail entities")
+    tail: str = Field(
+        description=(
+            "extracted tail entity like Microsoft, Apple, John. "
+            "Must use human-readable unique identifier."
+        )
+    )
+    tail_type: str = Field(
+        description="type of the extracted tail entity like Person, Company, etc"
+    )
