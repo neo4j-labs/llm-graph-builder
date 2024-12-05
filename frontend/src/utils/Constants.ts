7b01d357a6bb38ad30b6a79b1bd2d84de4f3a483
Prakriti Solankey <156313631+prakriti-solankey@users.noreply.github.com>
2024-12-05 08:50:02 +0000

    elementid if no id is there

--- frontend/src/utils/Constants.ts
+++ frontend/src/utils/Constants.ts
@@ -366,5 +366,5 @@ export const metricsinfo: Record<string, string> = {
   answer_relevancy: "Determines How well the answer addresses the user's question.",
   rouge_score: 'Determines How much the generated answer matches the reference answer, word-for-word.',
   semantic_score: 'Determines How well the generated answer understands the meaning of the reference answer.',
-  context_entity_recall_score: 'Determines the recall of entities present in both generated answer and retrieved contexts',
+  context_entity_recall: 'Determines the recall of entities present in both generated answer and retrieved contexts',
 };
