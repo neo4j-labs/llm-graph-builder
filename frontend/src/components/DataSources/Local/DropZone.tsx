b9a551a6c3e32c91c653393e590073c60248139d
kartikpersistent <101251502+kartikpersistent@users.noreply.github.com>
2024-12-17 06:42:57 +0000

    added xlxs format support

--- frontend/src/components/DataSources/Local/DropZone.tsx
+++ frontend/src/components/DataSources/Local/DropZone.tsx
@@ -217,7 +217,7 @@ const DropZone: FunctionComponent = () => {
                   text={
                     <Typography variant='body-small'>
                       <Flex gap='3' alignItems='flex-start'>
-                        <span>Microsoft Office (.docx, .pptx, .xls)</span>
+                        <span>Microsoft Office (.docx, .pptx, .xls, .xlsx)</span>
                         <span>PDF (.pdf)</span>
                         <span>Images (.jpeg, .jpg, .png, .svg)</span>
                         <span>Text (.html, .txt , .md)</span>
@@ -241,6 +241,7 @@ const DropZone: FunctionComponent = () => {
             'application/vnd.ms-powerpoint': ['.pptx'],
             'application/vnd.ms-excel': ['.xls'],
             'text/markdown': ['.md'],
+            'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet':['.xlsx']
           },
           onDrop: (f: Partial<globalThis.File>[]) => {
             onDropHandler(f);
