447e9f85751588c455cc47a32380898328b8976c
kaustubh-darekar <kaustubh_darekar@persistent.com>
2025-01-23 09:59:31 +0000

    removed debugging react strict mode

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,20 +2,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import Auth0ProviderWithHistory from './components/Auth/Auth.tsx';
-import React from 'react';
+//import React from 'react';
 import App from './App.tsx';
 import { SKIP_AUTH } from './utils/Constants.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      {SKIP_AUTH ? (
+  <BrowserRouter>
+    {SKIP_AUTH ? (
+      <App />
+    ) : (
+      <Auth0ProviderWithHistory>
         <App />
-      ) : (
-        <Auth0ProviderWithHistory>
-          <App />
-        </Auth0ProviderWithHistory>
-      )}
-    </BrowserRouter>
-  </React.StrictMode>
+      </Auth0ProviderWithHistory>
+    )}
+  </BrowserRouter>
 );
