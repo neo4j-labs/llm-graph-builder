d9a456ede9c007067078f8f505ab88676f41de1a
Prakriti Solankey <156313631+prakriti-solankey@users.noreply.github.com>
2025-01-23 09:52:53 +0000

    removed strict mode

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,12 +2,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import Auth0ProviderWithHistory from './components/Auth/Auth.tsx';
-import React from 'react';
 import App from './App.tsx';
 import { SKIP_AUTH } from './utils/Constants.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
     <BrowserRouter>
       {SKIP_AUTH ? (
         <App />
@@ -17,5 +15,4 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         </Auth0ProviderWithHistory>
       )}
     </BrowserRouter>
-  </React.StrictMode>
 );
