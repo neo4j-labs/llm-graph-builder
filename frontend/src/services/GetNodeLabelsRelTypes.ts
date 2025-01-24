ef61dd399fe8b5c7960d8bc2a83a88147f28a609
Prakriti Solankey <156313631+prakriti-solankey@users.noreply.github.com>
2025-01-24 13:02:38 +0000

    formdata missing

--- frontend/src/services/GetNodeLabelsRelTypes.ts
+++ frontend/src/services/GetNodeLabelsRelTypes.ts
@@ -2,8 +2,9 @@ import { ServerData } from '../types';
 import api from '../API/Index';
 
 export const getNodeLabelsAndRelTypes = async () => {
+  const formData = new FormData();
   try {
-    const response = await api.post<ServerData>(`/schema`);
+    const response = await api.post<ServerData>(`/schema`,formData);
     return response;
   } catch (error) {
     console.log(error);
