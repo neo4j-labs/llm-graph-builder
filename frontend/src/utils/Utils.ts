17ff72c634151bb3977031b0256d7f3898aba870
kartikpersistent <101251502+kartikpersistent@users.noreply.github.com>
2025-02-24 06:59:45 +0000

    some bad code

--- frontend/src/utils/Utils.ts
+++ frontend/src/utils/Utils.ts
@@ -146,7 +146,7 @@ export const getIcon = (node: any) => {
   }
   return undefined;
 };
-export function extractPdfFileName(url: string): boolean {
+export function extractPdfFileName(url: string): Record<string,string> {
   const splitUrl = url.split('/');
   const [encodedFileName] = splitUrl[splitUrl.length - 1].split('?');
   const decodedFileName = decodeURIComponent(encodedFileName);
