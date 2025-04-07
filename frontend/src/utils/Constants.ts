44743c030be1855b1c675e15d2cb8725b38999f3
kaustubh-darekar <kaustubh_darekar@persistent.com>
2025-04-07 16:01:16 +0530

    Gemini 2.5 model addition (#1221)
    * gemini 2.5 pro model added
    
    * Added gemini 2.5 pro

--- frontend/src/utils/Constants.ts
+++ frontend/src/utils/Constants.ts
@@ -1,5 +1,5 @@
 import { NvlOptions } from '@neo4j-nvl/base';
-import { GraphType, OptionType,PatternOption } from '../types';
+import { GraphType, OptionType, PatternOption } from '../types';
 import { getDateTime, getDescriptionForChatMode } from './Utils';
 import chatbotmessages from '../assets/ChatbotMessages.json';
 import schemaExamples from '../assets/newSchema.json';
@@ -12,28 +12,29 @@ export const llms =
   process.env?.VITE_LLM_MODELS?.trim() != ''
     ? (process.env.VITE_LLM_MODELS?.split(',') as string[])
     : [
-      'openai_gpt_4.5',
-      'openai-gpt-o3-mini',
-      'openai_gpt_4o',
-      'openai_gpt_4o_mini',
-      'gemini_1.5_pro',
-      'gemini_1.5_flash',
-      'gemini_2.0_flash',
-      'diffbot',
-      'azure_ai_gpt_35',
-      'azure_ai_gpt_4o',
-      'ollama_llama3',
-      'groq_llama3_70b',
-      'anthropic_claude_3_7_sonnet',
-      'fireworks_llama_v3p2_90b',
-      'fireworks_qwen72b_instruct',
-      'bedrock_claude_3_5_sonnet',
-      'bedrock_nova_micro_v1',
-      'bedrock_nova_lite_v1',
-      'bedrock_nova_pro_v1',
-      'fireworks_deepseek_r1',
-      'fireworks_deepseek_v3',
-    ];
+        'openai_gpt_4o',
+        'openai_gpt_4o_mini',
+        'openai-gpt-o3-mini',
+        'openai_gpt_4.5',
+        'gemini_1.5_pro',
+        'gemini_1.5_flash',
+        'gemini_2.0_flash',
+        'gemini_2.5_pro',
+        'diffbot',
+        'azure_ai_gpt_35',
+        'azure_ai_gpt_4o',
+        'ollama_llama3',
+        'groq_llama3_70b',
+        'anthropic_claude_3_7_sonnet',
+        'fireworks_llama_v3p2_90b',
+        'fireworks_qwen72b_instruct',
+        'bedrock_claude_3_5_sonnet',
+        'bedrock_nova_micro_v1',
+        'bedrock_nova_lite_v1',
+        'bedrock_nova_pro_v1',
+        'fireworks_deepseek_r1',
+        'fireworks_deepseek_v3',
+      ];
 
 export const supportedLLmsForRagas = [
   'openai_gpt_4.5',
@@ -43,6 +44,7 @@ export const supportedLLmsForRagas = [
   'gemini_1.5_pro',
   'gemini_1.5_flash',
   'gemini_2.0_flash',
+  'gemini_2.5_pro',
   'azure_ai_gpt_35',
   'azure_ai_gpt_4o',
   'groq_llama3_70b',
@@ -94,40 +96,40 @@ export const chatModeReadableLables: Record<string, string> = {
 export const chatModes =
   process.env?.VITE_CHAT_MODES?.trim() != ''
     ? process.env.VITE_CHAT_MODES?.split(',').map((mode) => ({
-      mode: mode.trim(),
-      description: getDescriptionForChatMode(mode.trim()),
-    }))
+        mode: mode.trim(),
+        description: getDescriptionForChatMode(mode.trim()),
+      }))
     : [
-      {
-        mode: chatModeLables.vector,
-        description: 'Performs semantic similarity search on text chunks using vector indexing.',
-      },
-      {
-        mode: chatModeLables.graph,
-        description: 'Translates text to Cypher queries for precise data retrieval from a graph database.',
-      },
-      {
-        mode: chatModeLables['graph+vector'],
-        description: 'Combines vector indexing and graph connections for contextually enhanced semantic search.',
-      },
-      {
-        mode: chatModeLables.fulltext,
-        description: 'Conducts fast, keyword-based search using full-text indexing on text chunks.',
-      },
-      {
-        mode: chatModeLables['graph+vector+fulltext'],
-        description: 'Integrates vector, graph, and full-text indexing for comprehensive search results.',
-      },
-      {
-        mode: chatModeLables['entity search+vector'],
-        description: 'Uses vector indexing on entity nodes for highly relevant entity-based search.',
-      },
-      {
-        mode: chatModeLables['global search+vector+fulltext'],
-        description:
-          'Use vector and full-text indexing on community nodes to provide accurate, context-aware answers globally.',
-      },
-    ];
+        {
+          mode: chatModeLables.vector,
+          description: 'Performs semantic similarity search on text chunks using vector indexing.',
+        },
+        {
+          mode: chatModeLables.graph,
+          description: 'Translates text to Cypher queries for precise data retrieval from a graph database.',
+        },
+        {
+          mode: chatModeLables['graph+vector'],
+          description: 'Combines vector indexing and graph connections for contextually enhanced semantic search.',
+        },
+        {
+          mode: chatModeLables.fulltext,
+          description: 'Conducts fast, keyword-based search using full-text indexing on text chunks.',
+        },
+        {
+          mode: chatModeLables['graph+vector+fulltext'],
+          description: 'Integrates vector, graph, and full-text indexing for comprehensive search results.',
+        },
+        {
+          mode: chatModeLables['entity search+vector'],
+          description: 'Uses vector indexing on entity nodes for highly relevant entity-based search.',
+        },
+        {
+          mode: chatModeLables['global search+vector+fulltext'],
+          description:
+            'Use vector and full-text indexing on community nodes to provide accurate, context-aware answers globally.',
+        },
+      ];
 
 export const chunkSize = process.env.VITE_CHUNK_SIZE ? Number(process.env.VITE_CHUNK_SIZE) : 1 * 1024 * 1024;
 export const tokenchunkSize = process.env.VITE_TOKENS_PER_CHUNK ? Number(process.env.VITE_TOKENS_PER_CHUNK) : 100;
@@ -354,7 +356,7 @@ export const appLabels = {
   predefinedSchema: 'Select a Pre-defined Schema',
   chunkingConfiguration: 'Select a Chunking Configuration',
   graphPatternTuple: 'Graph Pattern',
-  selectedPatterns: 'Selected Patterns'
+  selectedPatterns: 'Selected Patterns',
 };
 
 export const LLMDropdownLabel = {
@@ -364,9 +366,8 @@ export const LLMDropdownLabel = {
 export const getDefaultSchemaExamples = () => {
   return schemaExamples.map((example) => ({
     label: example.schema,
-    value: JSON.stringify(example.triplet)
-  })
-  )
+    value: JSON.stringify(example.triplet),
+  }));
 };
 
 export function mergeNestedObjects(objects: Record<string, Record<string, number>>[]) {
@@ -400,13 +401,6 @@ export const metricsinfo: Record<string, string> = {
 export const EXPIRATION_DAYS = 3;
 export const SKIP_AUTH = (process.env.VITE_SKIP_AUTH ?? 'true') == 'true';
 
-
-export const sourceOptions: PatternOption[] = [
-    { label: 'Person', value: 'Person' },
-];
-export const typeOptions: PatternOption[] = [
-    { label: 'WORKS_FOR', value: 'WORKS_FOR' },
-];
-export const targetOptions: PatternOption[] = [
-    { label: 'Company', value: 'Company' },
-];
+export const sourceOptions: PatternOption[] = [{ label: 'Person', value: 'Person' }];
+export const typeOptions: PatternOption[] = [{ label: 'WORKS_FOR', value: 'WORKS_FOR' }];
+export const targetOptions: PatternOption[] = [{ label: 'Company', value: 'Company' }];
