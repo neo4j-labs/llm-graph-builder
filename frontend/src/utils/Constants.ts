bf2d227ab7579f0231ae2d0c65ce7cd7ee5671dc
kaustubh-darekar <kaustubh_darekar@persistent.com>
2025-04-16 07:04:07 +0000

    new models of openai and fireworks llama4 added

--- frontend/src/utils/Constants.ts
+++ frontend/src/utils/Constants.ts
@@ -12,36 +12,41 @@ export const llms =
   process.env?.VITE_LLM_MODELS?.trim() != ''
     ? (process.env.VITE_LLM_MODELS?.split(',') as string[])
     : [
-      'openai-gpt-o3-mini',
-      'openai_gpt_4o',
-      'openai_gpt_4o_mini',
-      'openai_gpt_4.5',
-      'gemini_1.5_pro',
-      'gemini_1.5_flash',
-      'gemini_2.0_flash',
-      'gemini_2.5_pro',
-      'diffbot',
-      'azure_ai_gpt_35',
-      'azure_ai_gpt_4o',
-      'ollama_llama3',
-      'groq_llama3_70b',
-      'anthropic_claude_3_7_sonnet',
-      'fireworks_llama4_maverick',
-      'fireworks_llama_v3p2_90b',
-      'fireworks_qwen72b_instruct',
-      'bedrock_claude_3_5_sonnet',
-      'bedrock_nova_micro_v1',
-      'bedrock_nova_lite_v1',
-      'bedrock_nova_pro_v1',
-      'fireworks_deepseek_r1',
-      'fireworks_deepseek_v3',
-    ];
+        'openai_gpt_4o',
+        'openai_gpt_4o_mini',
+        'openai_gpt_4.1',
+        'openai_gpt_4.1-mini',
+        'openai-gpt-o3-mini',
+        'openai_gpt_4.5',
+        'gemini_1.5_pro',
+        'gemini_1.5_flash',
+        'gemini_2.0_flash',
+        'gemini_2.5_pro',
+        'diffbot',
+        'azure_ai_gpt_35',
+        'azure_ai_gpt_4o',
+        'ollama_llama3',
+        'groq_llama3_70b',
+        'anthropic_claude_3_7_sonnet',
+        'fireworks_llama4_maverick',
+        'fireworks_llama4_scout',
+        'fireworks_llama_v3p2_90b',
+        'fireworks_qwen72b_instruct',
+        'bedrock_claude_3_5_sonnet',
+        'bedrock_nova_micro_v1',
+        'bedrock_nova_lite_v1',
+        'bedrock_nova_pro_v1',
+        'fireworks_deepseek_r1',
+        'fireworks_deepseek_v3',
+      ];
 
 export const supportedLLmsForRagas = [
   'openai_gpt_4.5',
   'openai_gpt_4',
   'openai_gpt_4o',
   'openai_gpt_4o_mini',
+  'openai_gpt_4.1',
+  'openai_gpt_4.1-mini',
   'gemini_1.5_pro',
   'gemini_1.5_flash',
   'gemini_2.0_flash',
@@ -51,8 +56,8 @@ export const supportedLLmsForRagas = [
   'groq_llama3_70b',
   'anthropic_claude_3_7_sonnet',
   'fireworks_llama4_maverick',
-  'fireworks_llama_v3_70b',
-  'fireworks_llama4_instruct',
+  'fireworks_llama4_scout',
+  'fireworks_llama_v3p2_90b',
   'bedrock_claude_3_5_sonnet',
   'openai-gpt-o3-mini',
 ];
@@ -61,13 +66,15 @@ export const supportedLLmsForGroundTruthMetrics = [
   'openai_gpt_4',
   'openai_gpt_4o',
   'openai_gpt_4o_mini',
+  'openai_gpt_4.1',
+  'openai_gpt_4.1-mini',
   'azure_ai_gpt_35',
   'azure_ai_gpt_4o',
   'groq_llama3_70b',
   'anthropic_claude_3_7_sonnet',
   'fireworks_llama4_maverick',
-  'fireworks_llama_v3_70b',
-  'fireworks_llama4_instruct',
+  'fireworks_llama4_scout',
+  'fireworks_llama_v3p2_90b',
   'bedrock_claude_3_5_sonnet',
   'openai-gpt-o3-mini',
 ];
@@ -101,40 +108,40 @@ export const chatModeReadableLables: Record<string, string> = {
 export const chatModes =
   process.env?.VITE_CHAT_MODES?.trim() != ''
     ? process.env.VITE_CHAT_MODES?.split(',').map((mode) => ({
-      mode: mode.trim(),
-      description: getDescriptionForChatMode(mode.trim()),
-    }))
+        mode: mode.trim(),
+        description: getDescriptionForChatMode(mode.trim()),
+      }))
     : [
-      {
-        mode: chatModeLables.vector,
-        description: 'Performs semantic similarity search on text chunks using vector indexing.',
-      },
-      {
-        mode: chatModeLables.graph,
-        description: 'Translates text to Cypher queries for precise data retrieval from a graph database.',
-      },
-      {
-        mode: chatModeLables['graph+vector'],
-        description: 'Combines vector indexing and graph connections for contextually enhanced semantic search.',
-      },
-      {
-        mode: chatModeLables.fulltext,
-        description: 'Conducts fast, keyword-based search using full-text indexing on text chunks.',
-      },
-      {
-        mode: chatModeLables['graph+vector+fulltext'],
-        description: 'Integrates vector, graph, and full-text indexing for comprehensive search results.',
-      },
-      {
-        mode: chatModeLables['entity search+vector'],
-        description: 'Uses vector indexing on entity nodes for highly relevant entity-based search.',
-      },
-      {
-        mode: chatModeLables['global search+vector+fulltext'],
-        description:
-          'Use vector and full-text indexing on community nodes to provide accurate, context-aware answers globally.',
-      },
-    ];
+        {
+          mode: chatModeLables.vector,
+          description: 'Performs semantic similarity search on text chunks using vector indexing.',
+        },
+        {
+          mode: chatModeLables.graph,
+          description: 'Translates text to Cypher queries for precise data retrieval from a graph database.',
+        },
+        {
+          mode: chatModeLables['graph+vector'],
+          description: 'Combines vector indexing and graph connections for contextually enhanced semantic search.',
+        },
+        {
+          mode: chatModeLables.fulltext,
+          description: 'Conducts fast, keyword-based search using full-text indexing on text chunks.',
+        },
+        {
+          mode: chatModeLables['graph+vector+fulltext'],
+          description: 'Integrates vector, graph, and full-text indexing for comprehensive search results.',
+        },
+        {
+          mode: chatModeLables['entity search+vector'],
+          description: 'Uses vector indexing on entity nodes for highly relevant entity-based search.',
+        },
+        {
+          mode: chatModeLables['global search+vector+fulltext'],
+          description:
+            'Use vector and full-text indexing on community nodes to provide accurate, context-aware answers globally.',
+        },
+      ];
 
 export const chunkSize = process.env.VITE_CHUNK_SIZE ? Number(process.env.VITE_CHUNK_SIZE) : 1 * 1024 * 1024;
 export const tokenchunkSize = process.env.VITE_TOKENS_PER_CHUNK ? Number(process.env.VITE_TOKENS_PER_CHUNK) : 100;
