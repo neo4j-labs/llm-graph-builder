292737e1618466f6e5e1786ede9c1e2114e728f2
kartikpersistent <101251502+kartikpersistent@users.noreply.github.com>
2025-04-18 13:09:39 +0000

    fix: build error

--- frontend/src/components/Popups/ConnectionModal/VectorIndexMisMatchAlert.tsx
+++ frontend/src/components/Popups/ConnectionModal/VectorIndexMisMatchAlert.tsx
@@ -21,17 +21,19 @@ export default function VectorIndexMisMatchAlert({
   return (
     <Flex>
       <Box>
-        <Markdown className='whitespace-pre-wrap'>
-          {isVectorIndexAlreadyExists
-            ? `**Vector Index Incompatibility** 
+        <div className='whitespace-pre-wrap'>
+          <Markdown>
+            {isVectorIndexAlreadyExists
+              ? `**Vector Index Incompatibility** 
 The existing Neo4j vector index dimension (${userVectorIndexDimension}) is incompatible with the supported dimension (384) for this application. 
 To proceed, please choose one of the following options: 
 1.**Recreate Vector Index:** Click "Re-Create Vector Index" to generate a compatible vector index. 
 2.**Use a Different Instance:** Connect to a Neo4j instance with a compatible vector index configuration  `
-            : chunksExists
-            ? `A vector index is essential for performing efficient similarity searches within your data. Without it, some chunks of data will be invisible to queries based on meaning and context. Creating a vector index unlocks the full potential of your data by allowing you to find related information quickly and accurately.`
-            : ''}
-        </Markdown>
+              : chunksExists
+                ? `A vector index is essential for performing efficient similarity searches within your data. Without it, some chunks of data will be invisible to queries based on meaning and context. Creating a vector index unlocks the full potential of your data by allowing you to find related information quickly and accurately.`
+                : ''}
+          </Markdown>
+        </div>
       </Box>
       <div className='n-size-full n-flex n-flex-col n-items-center n-justify-center'>
         <ButtonWithToolTip
